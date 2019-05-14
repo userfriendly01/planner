91149c49b6354a7548c4788268f577bc8bf24ef7
Michael Nieman <michael.nieman@libertymutual.com>
2019-05-14 09:25:53 -0400

    Clean the slate

--- src/redux/actions/actions.js
+++
@@ -1,9 +0,0 @@
-import { TOGGLE_DRAWER, TOGGLE_MOBILE_DRAWER } from "./actionTypes";
-
-export const toggleDrawer = state => ({
-  type: TOGGLE_DRAWER
-});
-
-export const toggleMobileDrawer = state => ({
-  type: TOGGLE_MOBILE_DRAWER
-});
