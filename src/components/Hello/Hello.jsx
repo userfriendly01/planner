cd921161c6c7bd1f798c40efc2f6bad98deb5182
Michael Nieman <michael.nieman@libertymutual.com>
2019-07-23 16:20:55 -0400

    Implement button that returns express-ping-cookie-auth

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -1,8 +1,25 @@
+import axios from "axios";
 import React from "react";
 
+const myAxios = axios.create({ withCredentials: true });
+
 const Hello = () => {
+
+  const onClick = () => {
+    myAxios.get("http://localhost:8085/admin-login")
+      .then(res => {
+        console.log("<<<<<<<<<<<<<<<<<<<", res);
+      })
+      .catch(err => {
+        console.error("we're getting an axios error", err);
+      });
+  };
+
   return (
-    <div>This page is under construction. Click <span><a href="https://www.triton.lmig.com">here</a></span> to return to Triton</div>
+    <div>
+      This page is under construction. Click <span><a href="https://www.triton.lmig.com">here</a></span> to return to Triton
+      <button onClick={onClick}>Button</button>
+    </div>
   );
 };
 
