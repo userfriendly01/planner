fad3418910f60107e92dce05d6dec37b2734165a
Michael Nieman <michael.nieman@libertymutual.com>
2019-08-05 11:35:51 -0400

    Update port to 8080

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const port = 8084;
+const port = 8080;
 const serveDir = path.resolve(__dirname, "dist");
 const healthCheckEndpoint = "/health";
 
