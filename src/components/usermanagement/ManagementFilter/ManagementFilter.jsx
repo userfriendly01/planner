2edfbf84f73052e141bd4c39449b7d3a5e688d31
Michael Nieman <michael.nieman@libertymutual.com>
2019-08-20 13:56:58 -0400

    Remove console log

--- src/components/usermanagement/ManagementFilter/ManagementFilter.jsx
+++ src/components/usermanagement/ManagementFilter/ManagementFilter.jsx
@@ -84,18 +84,15 @@ const ManagementFilter = props => {
           }
         >
           <option value="">Show All</option>
-          {managers.map(manager => {
-            console.log(manager);
-            return (
-              <option
-                key={manager.manager_n_number}
-                value={manager.manager_n_number}
-              >
-                {manager.manager_first_name} {manager.manager_last_name}
-              </option>
-            );
-          }
-          )}
+          {managers.map(manager => (
+            <option
+              key={manager.manager_n_number}
+              value={manager.manager_n_number}
+            >
+              {manager.manager_first_name} {manager.manager_last_name}
+            </option>
+          ))}
+  )
         </Select>
       </FormControl>
       <CustomButton onClick={handleOpenAddManager}>Add Manager</CustomButton>
