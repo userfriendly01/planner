de4da82d655bb4b683b69d3d07ea7cb165ea4c7c
Klimaszewski <n0204972@lm.lmig.com>
2019-08-21 09:55:17 -0400

    tests

--- src/components/usermanagement/ModalPhoneNumber/test/ModalPhoneNumber.test.jsx
+++ src/components/usermanagement/ModalPhoneNumber/test/ModalPhoneNumber.test.jsx
@@ -1,4 +1,4 @@
-import ModalNNumber from "../ModalNNumber";
+import ModalPhoneNumber from "../ModalPhoneNumber";
 import React from "react";
 import {
   fireEvent,
@@ -7,41 +7,48 @@ import {
 
 const mockFunction = jest.fn();
 
-const renderComponent = (disabled, loading, nNumber) => {
-  return render(<ModalNNumber disabled={disabled} loading={loading} nNumber={nNumber} updateValue={mockFunction} />);
+const renderComponent = outgoing => {
+  return render(<ModalPhoneNumber outgoingNumber={outgoing} updateValue={mockFunction} />);
 };
 
-describe("<ModalNNumber />", () => {
+describe("<ModalPhoneNumber />", () => {
 
   beforeEach(() => {
     mockFunction.mockClear();
   });
 
   test("the initial state should be just an empty text field, with the correct label", () => {
-    const rendered = renderComponent(false, false, "");
-    expect(rendered.getByDisplayValue("")).toBeTruthy();
-    expect(rendered.getByLabelText("N Number")).toBeTruthy();
-    expect(rendered.queryAllByTestId("loading")).toHaveLength(0);
-    expect(rendered.getByLabelText("N Number")).not.toHaveClass("Mui-disabled");
-    expect(mockFunction.mock.calls.length).toBe(0);
+    const rendered = renderComponent("");
+    expect(rendered.getByLabelText("Outgoing Number")).toBeTruthy();
   });
 
   test("when we update the value, we should fire the event being sent to the component", () => {
-    const rendered = renderComponent(false, false, "N");
-    expect(rendered.getByDisplayValue("N")).toBeTruthy();
-    expect(rendered.getByLabelText("N Number")).toBeTruthy();
-    fireEvent.change(rendered.getByLabelText("N Number"), { target: { value: "N1" }});
+    const rendered = renderComponent("");
+    expect(rendered.getByLabelText("Outgoing Number")).toBeTruthy();
+    fireEvent.change(rendered.getByLabelText("Outgoing Number"), { target: { value: "123" }});
     expect(mockFunction.mock.calls.length).toBe(1);
-    expect(mockFunction.mock.calls[0][0]).toBe("N1");
   });
 
-  test("when we are doing a lookup on the field, we should validate that the spinner is showing", () => {
-    const rendered = renderComponent(false, true, "N1234567");
-    expect(rendered.queryAllByTestId("loading")).toHaveLength(1);
-  });
-
-  test("when disabled is true, the input should be disabled", () => {
-    const rendered = renderComponent(true, false, "N1234567");
-    expect(rendered.getByLabelText("N Number")).toHaveClass("Mui-disabled");
+  describe("when we update the value, the value should be masked correctly", () => {
+    test("the first few numbers should be wrapped in '()' with a space", () => {
+      const rendered = renderComponent("");
+      fireEvent.change(rendered.getByLabelText("Outgoing Number"), { target: { value: "123" }});
+      expect(mockFunction.mock.calls[0][0]).toBe("(123) ");
+    });
+    test("followed by the next 3 digits and a dash", () => {
+      const rendered = renderComponent("");
+      fireEvent.change(rendered.getByLabelText("Outgoing Number"), { target: { value: "123456" }});
+      expect(mockFunction.mock.calls[0][0]).toBe("(123) 456-");
+    });
+    test("and finally the last 4 digits", () => {
+      const rendered = renderComponent("");
+      fireEvent.change(rendered.getByLabelText("Outgoing Number"), { target: { value: "1234567890" }});
+      expect(mockFunction.mock.calls[0][0]).toBe("(123) 456-7890");
+    });
+    test("any additional digits should not be allowed", () => {
+      const rendered = renderComponent("");
+      fireEvent.change(rendered.getByLabelText("Outgoing Number"), { target: { value: "1234567890123465" }});
+      expect(mockFunction.mock.calls[0][0]).toBe("(123) 456-7890");
+    });
   });
 });
