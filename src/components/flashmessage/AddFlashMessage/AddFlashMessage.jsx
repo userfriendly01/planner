08bf70d7937d67b11271906436a1ed68d8cdb4b4
Michael Nieman <michael.nieman@libertymutual.com>
2019-11-27 11:56:04 -0500

    Pushing for Paolo

--- src/components/flashmessage/AddFlashMessage/AddFlashMessage.jsx
+++ src/components/flashmessage/AddFlashMessage/AddFlashMessage.jsx
@@ -2,11 +2,11 @@ import {
   useAdminDispatch,
   useAdminState
 } from "context";
-// import { apiPaths } from "globals";
+import { apiPaths } from "globals";
 import PropTypes from "prop-types";
 import React, { useState } from "react";
 import styled from "styled-components";
-// import { myAxios } from "utils";
+import { myAxios } from "utils";
 
 const AddMessageInput = styled.input`
   height: 200px;
@@ -24,7 +24,7 @@ const AddFlashMessage = props => {
   const flashMessage = state.flashMessage;
   const [charCount, setCharCount] = useState(0);
 
-  // const nNumber = "n0274027";
+  const nNumber = "n0274027";
 
   const handleChange = event => {
     dispatch({
@@ -38,8 +38,13 @@ const AddFlashMessage = props => {
     event.preventDefault();
     const popUp = confirm("Are you sure you want to create this flash message?");
     if (popUp === true) {
-      // myAxios.put(apiPaths.UPDATE_FLASH_MESSAGE, flashMessage, nNumber)
-      //   .then(res => console.log("response:", res));
+      // const req = {
+      //   callflowId: "whatever",
+      //   flashMessage: "flash!",
+      //   updatedBy: "n0132412"
+      // };
+      myAxios.put(apiPaths.UPDATE_FLASH_MESSAGE, flashMessage, nNumber) // this might have to change
+        .then(res => console.log("response:", res));
       toggleReadOnly(true);
     }
   };
