5369cedba34536ff399badb9b7791cea13629e98
Michael Nieman <michael.nieman@libertymutual.com>
2019-12-04 14:29:37 -0500

    Test AddFlashMessage

--- src/components/flashmessage/AddFlashMessage/test/AddFlashMessage.test.jsx
+++ src/components/flashmessage/AddFlashMessage/test/AddFlashMessage.test.jsx
@@ -1,20 +1,10 @@
 import AddFlashMessage from "../AddFlashMessage";
-import { StyledButton } from "components";
 import { initialState } from "context";
 import React from "react";
 import {
-  // act,
-  expectMockedComponent,
-  expectOnlyPassedProps,
-  // getTestState,
-  render,
-  setupMockedComponents
+  fireEvent,
+  render
 } from "testUtils";
-
-jest.mock("components", () => ({
-  __esModule: true,
-  StyledButton: jest.fn()
-}));
 
 describe("<AddFlashMessage />", () => {
 
@@ -24,43 +14,73 @@ describe("<AddFlashMessage />", () => {
     validMessage: false
   };
 
+  const mockToggleReadOnly = jest.fn();
+
   beforeEach(() => {
-    setupMockedComponents({ StyledButton });
+    mockToggleReadOnly.mockClear();
   });
 
   describe("initial state", () => {
     const state = initialTestState;
-    test("Should render input with value of empty string & charCount should be 0; should not render special character warning; button should be disabled", () => {
+    test("Should render input with value of empty string & charCount of 0; should NOT render special character warning; button should be disabled", () => {
       const rendered = render(<AddFlashMessage />, state);
-      expect(state.flashMessage).toBe("");
-      expect(state.charCount).toBe(0);
+      const button = rendered.getByText(/Add Message/);
+      const input = rendered.getByTestId("add-message-input");
+      expect(input.value).toBe("");
+      expect(rendered.container).toHaveTextContent("Characters: 0 / 1024");
       expect(rendered.container).not.toHaveTextContent("Special characters are not allowed");
-      expectMockedComponent(rendered, { StyledButton });
-      expectOnlyPassedProps(StyledButton, { disabled: true });
+      expect(button).toBeDisabled();
     });
   });
 
   describe("invalid character is entered into the form", () => {
-    const state = {
-      ...initialTestState,
-      flashMessage: "I contain special characters such as < ' > &"
-    };
     test("should display special character warning and button should be disabled", () => {
-      const rendered = render(<AddFlashMessage />, state);
+      const rendered = render(<AddFlashMessage />, initialTestState);
       const input = rendered.getByTestId("add-message-input");
-      
-      // expect(state.validMessage).toBe(false);
-      // expect(rendered.container).toHaveTextContent("Special characters are not allowed");
+      const button = rendered.getByText(/Add Message/);
+      fireEvent.change(input, { target: { value: "I contain special characters such as < ' > &" }});
+      expect(rendered.container).toHaveTextContent("Characters: 44 / 1024");
+      expect(rendered.container).toHaveTextContent("Special characters are not allowed");
+      expect(button).toBeDisabled();
     });
   });
 
   describe("valid message is entered", () => {
-    test("button should be enabled", () => {
-      //
+    const validMessage = "Hello! My name is Valid Message.";
+    test("button should be enabled; when submit button is clicked should display confirmation alert", () => {
+      window.confirm = jest.fn();
+      const rendered = render(<AddFlashMessage />, initialTestState);
+      const input = rendered.getByTestId("add-message-input");
+      const button = rendered.getByText(/Add Message/);
+      fireEvent.change(input, { target: { value: validMessage }});
+      expect(rendered.container).not.toHaveTextContent("Special characters are not allowed");
+      expect(button).not.toBeDisabled();
+      fireEvent.click(button);
+      expect(window.confirm).toHaveBeenCalledTimes(1);
+      expect(window.confirm).toHaveBeenCalledWith("Are you sure you want to create this flash message?");
     });
-    test("when submit button is clicked, should display confirmation alert", () => {
-      //
+    describe("when add message is confirmed", () => {
+      test("should toggle read only to true", () => {
+        window.confirm = jest.fn(() => true);
+        const rendered = render(<AddFlashMessage toggleReadOnly={mockToggleReadOnly} />, initialTestState);
+        const input = rendered.getByTestId("add-message-input");
+        const button = rendered.getByText(/Add Message/);
+        fireEvent.change(input, { target: { value: validMessage }});
+        fireEvent.click(button);
+        expect(mockToggleReadOnly).toHaveBeenCalledTimes(1);
+        expect(mockToggleReadOnly).toHaveBeenCalledWith(true);
+      });
+    });
+    describe("when add message is not confirmed", () => {
+      test("should not toggle read only", () => {
+        window.confirm = jest.fn(() => false);
+        const rendered = render(<AddFlashMessage toggleReadOnly={mockToggleReadOnly} />, initialTestState);
+        const input = rendered.getByTestId("add-message-input");
+        const button = rendered.getByText(/Add Message/);
+        fireEvent.change(input, { target: { value: validMessage }});
+        fireEvent.click(button);
+        expect(mockToggleReadOnly).not.toHaveBeenCalled();
+      });
     });
   });
-
 });
--- src/components/flashmessage/index.js
+++ src/components/flashmessage/index.js
@@ -2,4 +2,3 @@ export { default as AddFlashMessage } from "./AddFlashMessage/AddFlashMessage";
 export { default as ViewFlashMessage } from "./ViewFlashMessage/ViewFlashMessage";
 export { default as FlashMessageContainer } from "./FlashMessageContainer/FlashMessageContainer";
 export { default as FlashMessageSidebar } from "./FlashMessageSidebar/FlashMessageSidebar";
-export { default as SpecialCharacterWarning } from "./SpecialCharacterWarning/SpecialCharacterWarning";
