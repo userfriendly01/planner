451fce1e0e45568b509a20b8b1976e392464c5e8
Michael Nieman <michael.nieman@libertymutual.com>
2019-12-09 13:36:45 -0500

    Fix ViewFlashMessage test

--- src/components/flashmessage/ViewFlashMessage/test/ViewFlashMessage.test.jsx
+++ src/components/flashmessage/ViewFlashMessage/test/ViewFlashMessage.test.jsx
@@ -1,12 +1,17 @@
 import ViewFlashMessage from "../ViewFlashMessage";
+import MockAdapter from "axios-mock-adapter";
 import { initialState } from "context";
+import { apiPaths } from "globals";
 import React from "react";
 import {
+  act,
   fireEvent,
   mockStore,
   render
 } from "testUtils";
+import { myAxios } from "utils";
 
+const axiosMock = new MockAdapter(myAxios);
 const mockToggleReadOnly = jest.fn();
 
 const renderComponent = state => {
@@ -37,22 +42,47 @@ describe("<ViewFlashMessage />", () => {
     expect(mockToggleReadOnly.mock.calls).toHaveLength(1);
     expect(mockToggleReadOnly.mock.calls[0][0]).toBe(false);
   });
-  describe("clicking the delete button should ask the user if they really want to", () => {
-    test("if they select yes we should toggle the readonly state to false, as well as dispatch an action to update the flashmessage to empty.", () => {
-      window.confirm = jest.fn(() => true);
-      const state = initialState;
-      state.flashMessage = "Stuff be down, enjoy the free time";
-      const rendered = renderComponent(state);
-      fireEvent.click(rendered.getByTitle("Delete"));
-      expect(mockToggleReadOnly.mock.calls).toHaveLength(1);
-      expect(mockToggleReadOnly.mock.calls[0][0]).toBe(false);
-      const actions = mockStore.getActions();
-      expect(actions).toHaveLength(1);
-      expect(actions[0]).toEqual({
-        type: "updateFlashMessage",
-        payload: ""
+
+  describe("when view message is confirmed", () => {
+    beforeEach(() => window.confirm = () => true);
+    const state = {
+      ...initialState,
+      flashMessage: "Stuff be down, enjoy the free time"
+    };
+    describe("post to updateflashmessage is successful", () => {
+      beforeEach(() => axiosMock.onPost(apiPaths.UPDATE_FLASH_MESSAGE).reply(200, { whatever: "delete successful" }));
+      test("should return ... and toggle read only to true", done => {
+        const rendered = renderComponent(state);
+        const button = rendered.getByTitle("Delete");
+        act(() => {
+          fireEvent.click(button);
+          return Promise.resolve();
+        })
+          .then(() => {
+            expect(mockToggleReadOnly).toHaveBeenCalledTimes(1);
+            expect(mockToggleReadOnly).toHaveBeenCalledWith(false);
+            const actions = mockStore.getActions();
+            expect(actions).toHaveLength(1);
+            expect(actions[0]).toEqual({
+              type: "updateFlashMessage",
+              payload: ""
+            });
+            done();
+          });
       });
     });
+    describe("post to updateflashmessage fails", () => {
+      beforeEach(() => axiosMock.onPost(apiPaths.UPDATE_FLASH_MESSAGE).reply(500, { ohNo: "it failed" }));
+      test("should return ... and toggle read only should remain true", () => {
+        const rendered = renderComponent(state);
+        const button = rendered.getByTitle("Delete");
+        act(() => fireEvent.click(button));
+        expect(mockToggleReadOnly).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe("view message is not confirmed", () => {
     test("if they select no we should do nothing.", () => {
       window.confirm = jest.fn(() => false);
       const state = initialState;
