bb3435f6e84f282651470fb72639fbe143a974f4
n0263786 <faith.cuneo@libertymutual.com>
2020-01-06 10:12:45 -0500

    updated mini typo

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,6 @@ export * from "./formatWorkerSkillsToHTML";
 export * from "./getUniqueManagerList";
 export * from "./isErrorIn400s";
 export * from "./myAxios";
-export * from "./nameSkillsAndOfficeFilter";
+export * from "./filterTable";
 export * from "./skillsUtils";
 export * from "./sortUtils";
