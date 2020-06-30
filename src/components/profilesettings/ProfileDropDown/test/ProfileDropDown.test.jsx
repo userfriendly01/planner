f2475384ebbf195343d064f7e88715f324f0182b
Michael Nieman <michael.nieman@libertymutual.com>
2020-06-30 15:18:10 -0400

    Test ProfileDropDown

--- src/components/profilesettings/ProfileDropDown/test/ProfileDropDown.test.jsx
+++ src/components/profilesettings/ProfileDropDown/test/ProfileDropDown.test.jsx
@@ -1,1 +1,52 @@
-test("", () => expect(1).toBe(1));
+import ProfileDropDown from "../ProfileDropDown";
+import { OutlinedSelect } from "components";
+import React from "react";
+import {
+  expectMockedComponent,
+  render,
+  setupMockedComponents
+} from "testUtils";
+
+jest.mock("components", () => ({
+  __esModule: true,
+  OutlinedSelect: jest.fn()
+}));
+
+describe("<ProfileDropDown />", () => {
+  const profileFromParent = {
+    profileId: 2,
+    dialList: []
+  };
+  const availableProfiles = [
+    {
+      profile_id: 1,
+      profile_nme: "First Profile"
+    },
+    {
+      profile_id: 2,
+      profile_nme: "Second Profile"
+    },
+    {
+      profile_id: 3,
+      profile_nme: "Third Profile"
+    }
+  ];
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setupMockedComponents({ OutlinedSelect });
+  });
+  test("should render OutlinedSelect with correct props", () => {
+    const rendered = render(<ProfileDropDown
+      availableProfiles={availableProfiles}
+      profile={profileFromParent}
+    />);
+    expectMockedComponent(rendered, { OutlinedSelect }, 1);
+    const optionsDisplayFunc = OutlinedSelect.mock.calls[0][0].optionsDisplayFunc;
+    const option = optionsDisplayFunc(availableProfiles[1]);
+    expect(option).toEqual({
+      display: "2 - Second Profile",
+      key: 2,
+      value: 2
+    });
+  });
+});
