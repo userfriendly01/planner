bea3e2f9ad0a0dc1c8c323d6def8b2ce7a349fe7
Klimaszewski <DAVID.KLIMASZEWSKI@LibertyMutual.com>
2020-06-30 14:51:29 -0400

    CustomInput tested

---
+++ src/components/core/CustomInput/test/CustomInput.test.jsx
@@ -0,0 +1,179 @@
+import { CustomInput } from "../CustomInput";
+import { TextField } from "@material-ui/core";
+import { ModalFetchingRing } from "components";
+import React from "react";
+import {
+  getMockedComponentProps,
+  render,
+  setupMockedComponents
+} from "testUtils";
+
+jest.mock("@material-ui/core", () => ({
+  __esModule: true,
+  TextField: jest.fn()
+}));
+
+jest.mock("components", () => ({
+  __esModule: true,
+  ModalFetchingRing: jest.fn()
+}));
+
+const mockServiceFunction = jest.fn();
+const mockValidator = jest.fn();
+const updateValueFunc = jest.fn();
+
+describe("CustomInput", () => {
+
+  beforeEach(() => {
+    setupMockedComponents({
+      TextField,
+      ModalFetchingRing
+    });
+    jest.clearAllMocks();
+    mockValidator.mockImplementation(val => val === "validated");
+  });
+
+  const renderWithProps = ({
+    disabled, label, maxLength, name, value, validator, validatedService
+  }) => {
+    return render(<CustomInput
+      disabled={disabled}
+      label={label}
+      maxLength={maxLength}
+      name={name}
+      updateValue={updateValueFunc}
+      validator={validator}
+      validatedServiceCall={validatedService}
+      value={value} />);
+  };
+
+  test("we should pass the correct props to the TextField = false", () => {
+    const disabled = false;
+    const label = "test";
+    const maxLength = "5";
+    const name = test;
+    const value = "";
+    renderWithProps({
+      disabled,
+      label,
+      maxLength,
+      name,
+      value
+    });
+    const props = getMockedComponentProps(TextField);
+    expect(props.disabled).toBe(disabled);
+    expect(props.label).toBe(label);
+    expect(props.id).toBe(`outlined-${name}-input`);
+    expect(props.inputProps).toEqual({ maxLength });
+    expect(props.name).toBe(name);
+    expect(props.value).toBe(value);
+  });
+
+  test("non required fields should be fine if not sent", () => {
+    const label = "test";
+    const value = "";
+    renderWithProps({
+      disabled: null,
+      label,
+      maxLength: null,
+      name: null,
+      value
+    });
+    const props = getMockedComponentProps(TextField);
+    expect(props.disabled).toBe(false);
+    expect(props.label).toBe(label);
+    expect(props.id).toBe(null);
+    expect(props.inputProps).toEqual({});
+    expect(props.name).toBe(null);
+    expect(props.value).toBe(value);
+  });
+
+  describe("there is no validator", () => {
+    beforeEach(() => {
+      renderWithProps({
+        disabled: false,
+        label: "test",
+        maxLength: "5",
+        name: test,
+        value: "",
+        validator: undefined,
+        validatedService: undefined
+      });
+    });
+    test("we should still successfully update the value", () => {
+      const props = getMockedComponentProps(TextField);
+      const change = props.onChange;
+      change({
+        target: {
+          value: "validated"
+        }
+      });
+      expect(updateValueFunc).toHaveBeenCalledWith("validated");
+    });
+  });
+
+  describe("there is a validator", () => {
+    describe("there is no service call", () => {
+      beforeEach(() => {
+        renderWithProps({
+          disabled: false,
+          label: "test",
+          maxLength: "5",
+          name: test,
+          value: "",
+          validator: mockValidator,
+          validatedService: undefined
+        });
+      });
+      test("if the validator passes we should not throw an error by having no service", () => {
+        const props = getMockedComponentProps(TextField);
+        const change = props.onChange;
+        change({
+          target: {
+            value: "validated"
+          }
+        });
+        expect(updateValueFunc).toHaveBeenCalledWith("validated");
+        expect(mockValidator).toHaveBeenCalledWith("validated");
+      });
+    });
+    describe("there is a service call", () => {
+      beforeEach(() => {
+        renderWithProps({
+          disabled: false,
+          label: "test",
+          maxLength: "5",
+          name: test,
+          value: "",
+          validator: mockValidator,
+          validatedService: mockServiceFunction
+        });
+      });
+      test("if the validator fails we should not call the service", () => {
+        const props = getMockedComponentProps(TextField);
+        const change = props.onChange;
+        change({
+          target: {
+            value: "valid"
+          }
+        });
+        expect(updateValueFunc).toHaveBeenCalledWith("valid");
+        expect(mockValidator).toHaveBeenCalledWith("valid");
+        expect(mockServiceFunction).toHaveBeenCalledTimes(0);
+      });
+      test("if the validator passes we should call the service", () => {
+        mockServiceFunction.mockResolvedValue("all set");
+        const props = getMockedComponentProps(TextField);
+        const change = props.onChange;
+        change({
+          target: {
+            value: "validated"
+          }
+        });
+        expect(updateValueFunc).toHaveBeenCalledWith("validated");
+        expect(mockValidator).toHaveBeenCalledWith("validated");
+        expect(mockServiceFunction).toHaveBeenCalledWith("validated");
+      });
+    });
+  });
+});
