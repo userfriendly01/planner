b9aebda69edde9d25a87108b043e3bd0229dc100
Michael Nieman <michael.nieman@libertymutual.com>
2020-07-09 15:10:30 -0400

    Test DialListEntryForm

--- src/components/profilesettings/DialListEntryForm/test/DialListEntryForm.test.jsx
+++ src/components/profilesettings/DialListEntryForm/test/DialListEntryForm.test.jsx
@@ -26,7 +26,6 @@ jest.mock("components", () => ({
   StyledButton: jest.fn()
 }));
 
-
 const renderComponent = contactInfo => render(<DialListEntryForm contactInfo={contactInfo} handleClose={jest.fn()} />);
 
 describe("<DialListEntryForm />", () => {
@@ -49,11 +48,11 @@ describe("<DialListEntryForm />", () => {
       external_num: "900-555-1212"
     };
     describe("initial render", () => {
-      test("should render 2 ModalPhoneNumber components, 1 Text Field, and 2 buttons", () => {
+      test("should render 1 ModalPhoneNumber component, 2 Text Field components, and 2 buttons", () => {
         const rendered = renderComponent(contactInfo);
+        expectMockedComponent(rendered, { ModalPhoneNumber }, 1);
+        expectMockedComponent(rendered, { TextField }, 2);
         expectMockedComponent(rendered, { StyledButton }, 2);
-        expectMockedComponent(rendered, { ModalPhoneNumber }, 2);
-        expectMockedComponent(rendered, { TextField }, 1);
       });
     });
 
@@ -61,22 +60,51 @@ describe("<DialListEntryForm />", () => {
       test("initial state", () => {
         renderComponent(contactInfo);
         const expectedOutgoingProps = {
-          id: "transfer-number",
+          id: "transfer-number-input",
           number: contactInfo.contact_num,
           label: "Transfer Number"
         };
-        expectOnlyPassedProps(ModalPhoneNumber, expectedOutgoingProps, 0);
+        expectOnlyPassedProps(ModalPhoneNumber, expectedOutgoingProps);
       });
 
       test("changes made to the Transfer Number field", () => {
+        const newValue = "12345678";
         renderComponent(contactInfo);
         act(() => {
           const updateValue = ModalPhoneNumber.mock.calls[0][0].updateValue;
-          updateValue("12345678");
+          updateValue(newValue);
         });
-        expect(ModalPhoneNumber.mock.calls.length).toBe(4);
-        const newValue = ModalPhoneNumber.mock.calls[2][0].number;
-        expect(newValue).toEqual("12345678");
+        expect(ModalPhoneNumber.mock.calls.length).toBe(2);
+        const updatedTransferNumber = ModalPhoneNumber.mock.calls[1][0].number;
+        expect(updatedTransferNumber).toEqual(newValue);
+      });
+    });
+
+    describe("the Friendly Name field", () => {
+      test("initial state", () => {
+        renderComponent(contactInfo);
+        const expectedOutgoingProps = {
+          id: "friendly-name-input",
+          value: contactInfo.contact_nme,
+          label: "Friendly Name"
+        };
+        expect(TextField.mock.calls.length).toBe(2);
+        expectOnlyPassedProps(TextField, expectedOutgoingProps, 0);
+      });
+      test("changes made to the Friendly Name field", () => {
+        const newValue = "Hubie Brooks";
+        renderComponent(contactInfo);
+        act(() => {
+          const event = {
+            preventDefault() {},
+            target: { value: newValue }
+          };
+          const onChange = TextField.mock.calls[0][0].onChange;
+          onChange(event);
+        });
+        expect(TextField.mock.calls.length).toBe(4);
+        const updatedFriendlyName = TextField.mock.calls[2][0].value;
+        expect(updatedFriendlyName).toEqual(newValue);
       });
     });
 
@@ -84,32 +112,62 @@ describe("<DialListEntryForm />", () => {
       test("initial state", () => {
         renderComponent(contactInfo);
         const expectedOutgoingProps = {
-          id: "external-number",
-          number: contactInfo.external_num,
+          id: "external-number-input",
+          value: contactInfo.external_num,
           label: "External Number"
         };
-        expectOnlyPassedProps(ModalPhoneNumber, expectedOutgoingProps, 1);
+        expect(TextField.mock.calls.length).toBe(2);
+        expectOnlyPassedProps(TextField, expectedOutgoingProps, 1);
       });
-
       test("changes made to the External Number field", () => {
+        const newValue = "1-800-867-5309";
         renderComponent(contactInfo);
         act(() => {
-          const updateValue = ModalPhoneNumber.mock.calls[1][0].updateValue;
-          updateValue("87654321");
+          const event = {
+            preventDefault() {},
+            target: { value: newValue }
+          };
+          const onChange = TextField.mock.calls[1][0].onChange;
+          onChange(event);
         });
-        expect(ModalPhoneNumber.mock.calls.length).toBe(4);
-        const newValue = ModalPhoneNumber.mock.calls[3][0].number;
-        expect(newValue).toEqual("87654321");
+        expect(TextField.mock.calls.length).toBe(4);
+        const updatedExternalNumber = TextField.mock.calls[3][0].value;
+        expect(updatedExternalNumber).toEqual(newValue);
       });
     });
-
   });
 
-  // describe("contactInfo does not include external number", () => {
-  //   const contactInfo = {
-  //     contact_id: 20,
-  //     contact_nme: "Michael Jack Schmidt",
-  //     contact_num: "800-123-4569"
-  //   };
-  // });
+  describe("required contact info is missing", () => {
+    const contactInfo = {
+      whatever: "not contact_id",
+      somethingElse: "not contact_nme",
+      not_contact_num: "800-123-4567",
+      contact_id: 2000
+    };
+    test("should render all 3 fields with empty strings", () => {
+      const rendered = renderComponent(contactInfo);
+      expectMockedComponent(rendered, { ModalPhoneNumber }, 1);
+      expectMockedComponent(rendered, { TextField }, 2);
+      expectMockedComponent(rendered, { StyledButton }, 2);
+      const transferNumberOutgoingProps = {
+        id: "transfer-number-input",
+        number: "",
+        label: "Transfer Number"
+      };
+      expectOnlyPassedProps(ModalPhoneNumber, transferNumberOutgoingProps);
+      const friendlyNameOutgoingProps = {
+        id: "friendly-name-input",
+        value: "",
+        label: "Friendly Name"
+      };
+      expectOnlyPassedProps(TextField, friendlyNameOutgoingProps, 0);
+      const externalNumberOutgoingProps = {
+        id: "external-number-input",
+        value: "",
+        label: "External Number"
+      };
+      expect(TextField.mock.calls.length).toBe(2);
+      expectOnlyPassedProps(TextField, externalNumberOutgoingProps, 1);
+    });
+  });
 });
