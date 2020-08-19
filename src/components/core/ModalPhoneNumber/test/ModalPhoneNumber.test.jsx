08d561ba18a59e503495a99bbf073af4aeee5e7b
kyle.libby@libertymutual.com <kyle.libby@libertymutual.com>
2020-08-19 14:48:42 -0400

    added error, helperText, etc to modalPhoneNumber solid progress on unit tests

--- src/components/core/ModalPhoneNumber/test/ModalPhoneNumber.test.jsx
+++ src/components/core/ModalPhoneNumber/test/ModalPhoneNumber.test.jsx
@@ -1,54 +1,118 @@
 import ModalPhoneNumber from "../ModalPhoneNumber";
 import React from "react";
 import {
+  act,
   fireEvent,
   render
 } from "testUtils";
 
-const mockFunction = jest.fn();
+const updateValue = jest.fn();
 
-const renderComponent = outgoing => {
-  return render(<ModalPhoneNumber id={"outgoing-id"} number={outgoing} label="Outgoing Number" updateValue={mockFunction} />);
+const id = "outgoing-id";
+const label = "Outgoing Number";
+
+const renderComponent = ({
+  allowSevenDigitVdn,
+  error,
+  helperText,
+  onBlur,
+  showError
+}) => {
+  return render(
+    <ModalPhoneNumber
+      allowSevenDigitVdn={allowSevenDigitVdn}
+      error={error}
+      helperText={helperText}
+      id={id}
+      number={""}
+      onBlur={onBlur}
+      label={label}
+      showError={showError}
+      updateValue={updateValue}
+    />
+  );
 };
 
 describe("<ModalPhoneNumber />", () => {
 
   beforeEach(() => {
-    mockFunction.mockClear();
+    updateValue.mockClear();
   });
+
+  const changeNumberInput = (rendered, value) => {
+    act(() => {
+      fireEvent.change(rendered.getByLabelText(label), { target: { value }});
+    });
+  };
 
   test("the initial state should be just an empty text field, with the correct label", () => {
-    const rendered = renderComponent("");
-    expect(rendered.getByLabelText("Outgoing Number *")).toBeTruthy();
+    const rendered = renderComponent({
+      label: ""
+    });
+    expect(rendered.getByLabelText("Outgoing Number")).toBeTruthy();
   });
 
-  test("when we update the value, we should fire the event being sent to the component", () => {
-    const rendered = renderComponent("");
-    expect(rendered.getByLabelText("Outgoing Number *")).toBeTruthy();
-    fireEvent.change(rendered.getByLabelText("Outgoing Number *"), { target: { value: "123" }});
-    expect(mockFunction.mock.calls.length).toBe(1);
-  });
+  describe("allowSevenDigitVdn is true", () => {
+    const allowSevenDigitVdn = true;
 
-  describe("when we update the value, the value should be masked correctly", () => {
-    test("the first few numbers should be wrapped in '()' with a space", () => {
-      const rendered = renderComponent("");
-      fireEvent.change(rendered.getByLabelText("Outgoing Number *"), { target: { value: "123" }});
-      expect(mockFunction.mock.calls[0][0]).toBe("(123) ");
+    describe("component is set to 7 digit mode by clicking the toggle", () => {
+
+      const clickToggle = rendered => {
+        const toggleElement = rendered.getByTestId("toggle-seven-digit");
+        // there is an inner span and then input element within that
+        const toggleInputElement = toggleElement.children[0].children[0];
+        // expect(toggleInputElement).toEqual({})
+        act(() => {
+          fireEvent.click(toggleInputElement);
+        });
+      };
+
+      describe("updating input box values", () => {
+
+        test("should fire updateValue with correct args for various values", () => {
+          const rendered = renderComponent({
+            allowSevenDigitVdn
+          });
+          // toggle to use 7 digit mask and validation logic
+          clickToggle(rendered);
+          // initially updateValue and reset the value to empty string (happens when toggle is switched)
+          expect(updateValue.mock.calls[0]).toEqual(["", "", false]);
+          // invalid partial number
+          changeNumberInput(rendered, "603");
+          expect(updateValue.mock.calls[1]).toEqual(["603 ", "603", false]);
+          // invalid partial number
+          changeNumberInput(rendered, "603456");
+          expect(updateValue.mock.calls[2]).toEqual(["603 456", "603456", false]);
+          // valid number (VDN)
+          changeNumberInput(rendered, "6034567");
+          expect(updateValue.mock.calls[3]).toEqual(["603 4567", "6034567", true]);
+          // extra numbers omitted
+          changeNumberInput(rendered, "6034567890123123123");
+          expect(updateValue.mock.calls[4]).toEqual(["603 4567", "6034567", true]);
+        });
+      });
     });
-    test("followed by the next 3 digits and a dash", () => {
-      const rendered = renderComponent("");
-      fireEvent.change(rendered.getByLabelText("Outgoing Number *"), { target: { value: "123456" }});
-      expect(mockFunction.mock.calls[0][0]).toBe("(123) 456-");
-    });
-    test("and finally the last 4 digits", () => {
-      const rendered = renderComponent("");
-      fireEvent.change(rendered.getByLabelText("Outgoing Number *"), { target: { value: "1234567890" }});
-      expect(mockFunction.mock.calls[0][0]).toBe("(123) 456-7890");
-    });
-    test("any additional digits should not be allowed", () => {
-      const rendered = renderComponent("");
-      fireEvent.change(rendered.getByLabelText("Outgoing Number *"), { target: { value: "1234567890123465" }});
-      expect(mockFunction.mock.calls[0][0]).toBe("(123) 456-7890");
+
+    describe("component is set to 10 digit mode (by default)", () => {
+
+      describe("updating input box values", () => {
+
+        test("should fire updateValue with correct args for various values", () => {
+          const rendered = renderComponent({});
+          // invalid partial number
+          changeNumberInput(rendered, "603");
+          expect(updateValue.mock.calls[0]).toEqual(["(603) ", "603", false]);
+          // invalid partial number
+          changeNumberInput(rendered, "603456");
+          expect(updateValue.mock.calls[1]).toEqual(["(603) 456", "603456", false]);
+          // valid number
+          changeNumberInput(rendered, "6034567890");
+          expect(updateValue.mock.calls[2]).toEqual(["(603) 456-7890", "6034567890", true]);
+          // extra numbers omitted
+          changeNumberInput(rendered, "6034567890123123123");
+          expect(updateValue.mock.calls[3]).toEqual(["(603) 456-7890", "6034567890", true]);
+        });
+      });
     });
   });
 });
