5c893461608658df0b64e0395d9703f8d5e6e9b4
Michael Nieman <michael.nieman@libertymutual.com>
2020-10-22 11:46:23 -0400

    Cleanup Faith's mess :face_with_rolling_eyes:

--- src/components/usermanagement/ConfirmationModal/test/ConfirmationModal.test.jsx
+++ src/components/usermanagement/ConfirmationModal/test/ConfirmationModal.test.jsx
@@ -27,16 +27,16 @@ const mockHandleConfirm = jest.fn();
 const initialSaveResult = {
   status: null,
   message: null
-}
+};
 const body = {
   confirmationText: "Are you sure?",
   data: "Peter Griffin"
-}
+};
 
 const renderComponent = () => {
   return render(<ConfirmationModal
     handleClose={mockHandleClose}
-    onConfirm={mockHandleConfirm} 
+    onConfirm={mockHandleConfirm}
     body={body}
     saveResult={initialSaveResult} />);
 };
@@ -61,24 +61,24 @@ describe("<ConfirmationModal />", () => {
     });
   });
   describe("Confirm Button", () => {
-    test("When the Confirm Button is clicked, handleConfirm fails", () => {
+    test("When the Confirm Button is clicked, handleConfirm is called", () => {
       renderComponent();
       expectOnlyPassedProps(StyledButton, {
         onClick: mockHandleConfirm
       }, 0);
       const confirm = StyledButton.mock.calls[0][0].onClick;
-      act(() => confirm())
+      act(() => confirm());
       expect(mockHandleConfirm).toHaveBeenCalledTimes(1);
     });
   });
   describe("Cancel Button", () => {
-    test("When the Cancel Button is clicked, handleClose is run", () => {
+    test("When the Cancel Button is clicked, handleClose is called", () => {
       renderComponent();
       expectOnlyPassedProps(StyledButton, {
         onClick: mockHandleClose
       }, 1);
       const cancel = StyledButton.mock.calls[1][0].onClick;
-      act(() => cancel())
+      act(() => cancel());
       expect(mockHandleClose).toHaveBeenCalledTimes(1);
     });
   });
@@ -86,10 +86,10 @@ describe("<ConfirmationModal />", () => {
     const renderComponent = saveResult => {
       return render(<ConfirmationModal
         handleClose={mockHandleClose}
-        onConfirm={mockHandleConfirm} 
+        onConfirm={mockHandleConfirm}
         body={body}
         saveResult={saveResult} />);
-    }
+    };
     test("When the Save Result is successful, the ModalOverlay is passed the correct props, handleClose is run", () => {
       const rendered = renderComponent({
         status: "success",
