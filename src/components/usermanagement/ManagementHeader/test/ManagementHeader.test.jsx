5cd41a77566afe796e719992e81a30da16cf3046
kyle.libby@libertymutual.com <kyle.libby@libertymutual.com>
2020-10-27 15:17:09 -0400

    finished managementheader tests

--- src/components/usermanagement/ManagementHeader/test/ManagementHeader.test.jsx
+++ src/components/usermanagement/ManagementHeader/test/ManagementHeader.test.jsx
@@ -1,29 +1,32 @@
 import ManagementHeader from "../ManagementHeader";
 import {
-  AddUser,
   ManagerFilter,
   ResetSkillsButton,
   SearchBox
 } from "components";
+import { formModes } from "globals";
 import React from "react";
 import {
   expectMockedComponent,
+  expectOnlyPassedProps,
+  fireEvent,
+  getMockedComponentProps,
   render,
   setupMockedComponents
 } from "testUtils";
 
 jest.mock("components/usermanagement", () => ({
   __esModule: true,
-  AddUser: jest.fn(),
   ManagerFilter: jest.fn(),
   ResetSkillsButton: jest.fn(),
   SearchBox: jest.fn()
 }));
 
-const filterBy = "";
-const searchBy = "";
+const filterBy = "who cares";
+const searchBy = "i don't care";
 const setFilter = jest.fn();
 const setSearch = jest.fn();
+const setUserEntryFormState = jest.fn();
 
 const renderComponent = () => {
   return render(
@@ -32,24 +35,44 @@ const renderComponent = () => {
       searchBy={searchBy}
       setFilter={setFilter}
       setSearch={setSearch}
+      setUserEntryFormState={setUserEntryFormState}
     />);
 };
 
 describe("<ManagementHeader />", () => {
   beforeEach(() => {
+    jest.clearAllMocks();
     setupMockedComponents({
-      AddUser,
       ManagerFilter,
       ResetSkillsButton,
       SearchBox
     });
   });
 
-  test("upon initial render, should display Manager Filter & add user buttons, and all managers should be listed in the dropdown sorted by first name", () => {
+  test("upon initial render, should render ManagerFilter with correct props and SearchBox with correct props, and ResetSkillsButton", () => {
     const rendered = renderComponent();
-    expectMockedComponent(rendered, { AddUser });
+
     expectMockedComponent(rendered, { ManagerFilter });
+    expectOnlyPassedProps(ManagerFilter, { filterBy });
+    getMockedComponentProps(ManagerFilter).setFilter();
+    expect(setFilter).toHaveBeenCalledTimes(1);
+
     expectMockedComponent(rendered, { ResetSkillsButton });
+
     expectMockedComponent(rendered, { SearchBox });
+    expectOnlyPassedProps(SearchBox, { searchBy });
+    getMockedComponentProps(SearchBox).setSearch();
+    expect(setSearch).toHaveBeenCalledTimes(1);
+  });
+
+  test("when Add User button is clicked setUserEntryFormState is called", () => {
+    const rendered = renderComponent();
+    const addUserButtonElement = rendered.getByText("Add User");
+    fireEvent.click(addUserButtonElement);
+    expect(setUserEntryFormState).toHaveBeenCalledWith({
+      formMode: formModes.INSERT,
+      open: true,
+      worker: null
+    });
   });
 });
