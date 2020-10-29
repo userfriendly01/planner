9ffdbe790e7951066809e2263fdcbfc5ce92b0ab
kyle.libby@libertymutual.com <kyle.libby@libertymutual.com>
2020-10-29 08:54:04 -0400

    finished slight refactor of modalExtension

--- src/components/usermanagement/ModalExtension/test/ModalExtension.test.jsx
+++ src/components/usermanagement/ModalExtension/test/ModalExtension.test.jsx
@@ -7,6 +7,7 @@ import React from "react";
 import { checkExtension } from "services";
 import {
   act,
+  expectMockedComponent,
   getLastInstanceCalled,
   getMockedComponentProps,
   render,
@@ -25,21 +26,22 @@ jest.mock("services", () => ({
   checkExtension: jest.fn()
 }));
 
-const mockClearExtension = jest.fn();
-const mockSetForm = jest.fn();
-const mockUpdateValue = jest.fn();
+const originalExtension = "1234";
 
-const renderComponent = (disabled, form, extension, isEditExisting = true, error) => {
+const mockOnBlur = jest.fn();
+const mockOnClear = jest.fn();
+const mockOnUpdate = jest.fn();
+
+const renderComponent = (disabled, extension, error, originalValue) => {
   return render(<ModalExtension
-    clearExtension={mockClearExtension}
     disabled={disabled}
     error={error}
     extension={extension}
-    form={form}
-    originalValue={"1234"}
-    isEditExisting={isEditExisting}
-    setForm={mockSetForm}
-    updateValue={mockUpdateValue} />);
+    onBlur={mockOnBlur}
+    onClear={mockOnClear}
+    onUpdate={mockOnUpdate}
+    originalValue={originalValue}
+  />);
 };
 
 describe("<ModalExtension />", () => {
@@ -51,22 +53,29 @@ describe("<ModalExtension />", () => {
     jest.clearAllMocks();
   });
   describe("testing the CustomInput props", () => {
-    const form = {};
     test("the initial state should be just an empty text field, with the correct label, and no helper text", () => {
       const disabled = false;
       const extension = "";
-      renderComponent(disabled, form, extension);
-      const props = getMockedComponentProps(CustomInput);
-      expect(props.disabled).toBe(disabled);
-      expect(props.label).toBe("Extension");
-      expect(props.maxLength).toEqual("4");
-      expect(props.name).toBe("Extension");
-      expect(props.updateValue).toBe(mockUpdateValue);
-      expect(props.value).toBe(extension);
+      renderComponent(disabled, extension);
+
+      // CustomInput
+      const customInputProps = getMockedComponentProps(CustomInput);
+      expect(customInputProps.disabled).toBe(disabled);
+      expect(customInputProps.label).toBe("Extension");
+      expect(customInputProps.maxLength).toEqual("4");
+      expect(customInputProps.name).toBe("Extension");
+      const ext = "1111";
+      customInputProps.updateValue(ext);
+      expect(mockOnUpdate).toHaveBeenCalledWith(ext, false);
+      customInputProps.onBlur();
+      expect(mockOnBlur).toHaveBeenCalledWith();
+      expect(customInputProps.value).toBe(extension);
+
+      // ModalHelperText
       expect(ModalHelperText.mock.calls.length).toBe(0);
     });
     test("the validator sent in should enforce a strict Extension rule", () => {
-      renderComponent(false, form, "");
+      renderComponent(false, "");
       const props = getMockedComponentProps(CustomInput);
       const validator = props.validator;
       expect(validator("")).toBe(false);
@@ -77,129 +86,93 @@ describe("<ModalExtension />", () => {
       expect(validator("1234")).toEqual(true);
     });
     describe("the validated service call", () => {
-      test("should set the form on success with original extension", async () => {
+      test("should call onUpdate on success with original extension", async () => {
         checkExtension.mockResolvedValue(true);
-        renderComponent(false, form, "");
-        const extension = "1234";
+        renderComponent(false, "", false, originalExtension);
         const props = getMockedComponentProps(CustomInput);
         const validatedServiceCall = props.validatedServiceCall;
-        await act(() => validatedServiceCall(extension));
+        await act(() => validatedServiceCall(originalExtension));
         await waitFor(() => {
-          expect(checkExtension).toHaveBeenCalledWith(extension);
-          expect(mockSetForm).toHaveBeenCalledWith({
-            extension,
-            extensionValid: true,
-            extensionUpdated: false
-          });
+          expect(checkExtension).toHaveBeenCalledWith(originalExtension);
+          expect(mockOnUpdate).toHaveBeenCalledWith(originalExtension, true);
         });
       });
-      test("should set the form on success with non-original valid extension", async () => {
+      test("should call onUpdate on success with non-original valid extension", async () => {
         checkExtension.mockResolvedValue(true);
-        renderComponent(false, form, "1111");
+        renderComponent(false, "", false, originalExtension);
         const extension = "5678";
         const props = getMockedComponentProps(CustomInput);
         const validatedServiceCall = props.validatedServiceCall;
         await act(() => validatedServiceCall(extension));
         await waitFor(() => {
           expect(checkExtension).toHaveBeenCalledWith(extension);
-          expect(mockSetForm).toHaveBeenCalledWith({
-            extension,
-            extensionValid: true,
-            extensionUpdated: true
-          });
+          expect(mockOnUpdate).toHaveBeenCalledWith(extension, true);
         });
       });
       test("should set the form on success with no original and valid extension", async () => {
         checkExtension.mockResolvedValue(true);
-        renderComponent(false, form, "", false);
+        renderComponent(false, "", false);
         const extension = "5678";
         const props = getMockedComponentProps(CustomInput);
         const validatedServiceCall = props.validatedServiceCall;
         await act(() => validatedServiceCall(extension));
         await waitFor(() => {
           expect(checkExtension).toHaveBeenCalledWith(extension);
-          expect(mockSetForm).toHaveBeenCalledWith({
-            extension,
-            extensionValid: true,
-            extensionUpdated: true
-          });
+          expect(mockOnUpdate).toHaveBeenCalledWith(extension, true);
         });
       });
       test("should say invalid if null is returned", async () => {
         checkExtension.mockResolvedValue(null);
-        renderComponent(false, form, "");
+        renderComponent(false, "");
         const extension = "2222";
         const props = getMockedComponentProps(CustomInput);
         const validatedServiceCall = props.validatedServiceCall;
         await act(() => validatedServiceCall(extension));
         await waitFor(() => {
           expect(checkExtension).toHaveBeenCalledWith(extension);
-          expect(mockSetForm).toHaveBeenCalledWith({
-            extension,
-            extensionValid: false,
-            extensionUpdated: false
-          });
+          expect(mockOnUpdate).toHaveBeenCalledWith(extension, false);
         });
       });
       test("should notify the user of an error on failure", async () => {
         checkExtension.mockRejectedValue(false);
-        renderComponent(false, form, "");
+        renderComponent(false, "");
         const extension = "2222";
         const props = getMockedComponentProps(CustomInput);
         const validatedServiceCall = props.validatedServiceCall;
         await act(() => validatedServiceCall(extension));
         await waitFor(() => {
           expect(checkExtension).toHaveBeenCalledWith(extension);
-          expect(mockSetForm).toHaveBeenCalledWith({
-            extension,
-            extensionValid: false,
-            extensionUpdated: false
-          });
+          expect(mockOnUpdate).toHaveBeenCalledWith(extension, false);
         });
       });
     });
     describe("the modal helper", () => {
       test("should show when extension is validating", async () => {
         checkExtension.mockResolvedValue(true);
-        renderComponent(false, {
-          extension: "1111",
-          extensionValid: true,
-          extensionUpdated: true
-        }, "2222");
+        renderComponent(false, "2222");
         const props = getMockedComponentProps(CustomInput);
         const validatedServiceCall = props.validatedServiceCall;
         await act(() => validatedServiceCall("1234"));
         await waitFor(() => {
-          // get the render BEFORE the last because this will render once again due to this promise being called
+          // get render BEFORE the last one because it will re-render once more with empty message
           const helperProps = getMockedComponentProps(ModalHelperText, getLastInstanceCalled(ModalHelperText) - 1);
           expect(helperProps.error).toBe(false);
-          expect(helperProps.clearFunction).toBe(mockClearExtension);
           expect(helperProps.message).toEqual("Validating...");
         });
       });
-      test("should show when extension is valid", () => {
+      test("should show extension is valid in initial render if extension is valid 4 digits", () => {
         checkExtension.mockResolvedValue(true);
-        renderComponent(false, {
-          extension: "1111",
-          extensionValid: true,
-          extensionUpdated: true
-        }, "2222");
+        renderComponent(false, "2222");
         const props = getMockedComponentProps(ModalHelperText);
         expect(props.error).toBe(false);
-        expect(props.clearFunction).toBe(mockClearExtension);
+        props.clearFunction();
+        expect(mockOnClear).toHaveBeenCalledTimes(1);
         expect(props.message).toEqual("Extension is valid");
       });
-      test("should show when extension is not valid", () => {
+      test("should not show ModalHelperText in initial render if extension is not valid 4 digits", () => {
         checkExtension.mockResolvedValue(true);
-        renderComponent(false, {
-          extension: "1111",
-          extensionValid: false,
-          extensionUpdated: false
-        }, "2222");
-        const props = getMockedComponentProps(ModalHelperText);
-        expect(props.error).toBe(true);
-        expect(props.clearFunction).toBe(mockClearExtension);
-        expect(props.message).toEqual("Extension already in use");
+        const rendered = renderComponent(false, "");
+        expectMockedComponent(rendered, ModalHelperText, 0);
       });
     });
 
@@ -207,11 +180,7 @@ describe("<ModalExtension />", () => {
       const error = true;
 
       test("should render CustomInput with error flag", () => {
-        renderComponent(false, {
-          extension: "1111",
-          extensionValid: false,
-          extensionUpdated: false
-        }, "2222", true, error);
+        renderComponent(false, "2222", true);
         expect(getMockedComponentProps(CustomInput).error).toBe(error);
       });
     });
@@ -220,12 +189,8 @@ describe("<ModalExtension />", () => {
       const error = undefined;
 
       test("should render CustomInput with error flag", () => {
-        renderComponent(false, {
-          extension: "1111",
-          extensionValid: false,
-          extensionUpdated: false
-        }, "2222", true, error);
-        expect(getMockedComponentProps(CustomInput).error).toBe(undefined);
+        renderComponent(false, "2222", error);
+        expect(getMockedComponentProps(CustomInput).error).toBe(error);
       });
     });
   });
