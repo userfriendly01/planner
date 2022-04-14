fbde76f0faa41bd5f2418baf7622d2753255c155
n1511718 <Nolan.Price@libertymutual.com>
2022-04-14 15:24:24 -0700

    quick test

--- src/components/message/MessageContainer/MessageContainer.jsx
+++ src/components/message/MessageContainer/MessageContainer.jsx
@@ -64,6 +64,7 @@ const MessageContainer = props => {
   } = props;
 
   const adminState = useAdminState();
+  console.log("adminState", adminState);
   const nNumber = adminState.userContext.pingIdentity.sub;
 
   let loggedInWorker;
