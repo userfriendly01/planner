b3c61130210f15e60faf7dec24cf9afc2f479a65
Cuneo <Faith.Cuneo@LibertyMutual.com>
2022-04-15 20:43:53 -0400

    adding log

--- src/services/calabrio.ts
+++ src/services/calabrio.ts
@@ -4,6 +4,7 @@ import { myAxios } from "utils";
 
 export const getCalabrioOrg = async (): Promise<CalabrioGroup[]> => {
   const organization: any = await myAxios.get(apiPaths.GET_CALABRIO_ORG);
+  console.log("ORGANIZATION: ", organization);
   return organization.map((group: CalabrioGroup) => {
     delete group.agents;
     return group;
