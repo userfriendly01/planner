7e5e1856a8bcdab61ecd05659c87be25797faea1
Cuneo <Faith.Cuneo@LibertyMutual.com>
2022-04-28 12:54:35 -0400

    tweaks

--- src/components/usermanagement/CallRecording/CallRecordingScope.tsx
+++ src/components/usermanagement/CallRecording/CallRecordingScope.tsx
@@ -92,10 +92,7 @@ const CallRecordingScope = (props:any) => {
     const childrenTeams = identifyChildrenTeams(group.groupId);
     console.log("teams in teams", childrenTeams);
     console.log("childrenTeams in checkIfPartial", childrenTeams);
-    const checkedChildrenTeams = childrenTeams.filter((teamIndex: any) => {
-      console.log("Team", teams[teamIndex]);
-      teams[teamIndex].checked === true;
-    });
+    const checkedChildrenTeams = childrenTeams.filter((teamIndex: any) => teams[teamIndex].checked === true);
     console.log("checkedChildrenTeams in checkIfPartial", checkedChildrenTeams);
     if(childrenTeams.length !== 0 && checkedChildrenTeams.length !== childrenTeams.length && group.checked || childrenTeams.length !== 0 && checkedChildrenTeams.length === childrenTeams.length && !group.checked){
       return true;
