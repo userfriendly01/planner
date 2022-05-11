942f45b716e31baf0e6aea85bb014878549b1c9d
Cuneo <Faith.Cuneo@LibertyMutual.com>
2022-05-11 14:41:51 -0400

    yes

--- src/components/usermanagement/CallRecording/CallRecordingForm.tsx
+++ src/components/usermanagement/CallRecording/CallRecordingForm.tsx
@@ -131,7 +131,7 @@ const CallRecordingForm = (props: any) => {
           options={getTeamOptions()}
           updateValue={teamId => setForm({
             type: userFormActions.SET_CALABRIO_TEAM,
-            payload: teams.map(team => team.groupId === teamId)
+            payload: teams.filter(team => team.groupId === teamId)
           })}
           value={form.calabrioUser.team}
         />
