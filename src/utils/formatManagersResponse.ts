930a3b0a3620fb4dc5ea4c0df6a7ade26a807c15
Cuneo <Faith.Cuneo@LibertyMutual.com>
2022-06-21 16:04:54 -0400

    checking the conversion of the calabrio array

--- src/utils/formatManagersResponse.ts
+++ src/utils/formatManagersResponse.ts
@@ -4,14 +4,14 @@ import { DbManagerResponse } from "services";
 export const formatManagersResponse = (response: DbManagerResponse[]): Manager[] => {
   if (response) {
     return response.map<Manager>(manager => {
-      console.log("**Type of Manager's Calabrio Teams", typeof manager.calabrio_team_ids);
+      console.log("**Type of Manager's Calabrio Teams", typeof Array.from(manager.calabrio_team_ids), Array.from(manager.calabrio_team_ids));
       return {
         manager_first_name: manager.manager_first_nme,
         manager_last_name: manager.manager_last_nme,
         manager_n_number: manager.manager_n_num,
         manager_id: manager.manager_id,
         profile_id: manager.profile_id,
-        calabrio_team_ids: manager.calabrio_team_ids
+        calabrio_team_ids: Array.from(manager.calabrio_team_ids)
       };
     });
   } else {
