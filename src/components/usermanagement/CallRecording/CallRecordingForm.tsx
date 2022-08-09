f5ad69a02edccb2a40aa12288eb6acc43bf40ed9
Cuneo <Faith.Cuneo@LibertyMutual.com>
2022-08-09 10:56:34 -0400

    small tweak

--- src/components/usermanagement/CallRecording/CallRecordingForm.tsx
+++ src/components/usermanagement/CallRecording/CallRecordingForm.tsx
@@ -81,9 +81,10 @@ const CallRecordingForm = (props: CallRecordingFormInterface) => {
   const setScopeOnExistingUser = () => {
     const email = form.nNumberFetchedUser.email.toLowerCase();
     const userRecord = users.find(user => user.email?.toLowerCase() === email);
+    console.warn("userRecord", userRecord);
 
     if(userRecord){
-      getCalabrioUser(userRecord.personId).then((res: any) => {
+      getCalabrioUser(userRecord.id).then((res: any) => {
         const userGroups: any[] = [];
         const userTeams: any[] = [];
 
