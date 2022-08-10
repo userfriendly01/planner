6b224d71a5f9e87d85fd0245cce38f4c28134f1c
Charpentier <Gabriel.Charpentier@libertymutual.com>
2022-08-10 10:06:54 -0400

    improvement

--- src/components/usermanagement/ManagerModal/ManagerModal.tsx
+++ src/components/usermanagement/ManagerModal/ManagerModal.tsx
@@ -250,6 +250,12 @@ const ManagerModal = (props: ManagerModalProps) => {
               updateValue={(event: any, newInputValue: any) => {
                 if(newInputValue.some((t: any) => t.value === "add-team")){
                   handleOpenTeam();
+                  const index = newInputValue.findIndex((object: any) => {
+                    object.value === "add-team";
+                  });
+                  console.log(newInputValue);
+                  newInputValue.splice(index, 1);
+                  console.log(newInputValue);
                 }
                 setSelectedCalabrioTeams(newInputValue.map((team:any) => team.value));
               }}
