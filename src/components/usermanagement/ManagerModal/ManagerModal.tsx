1b6c01f83ec492d52644d824fc1a1685ae098c5f
Charpentier <Gabriel.Charpentier@libertymutual.com>
2022-08-12 14:13:54 -0400

    testing

--- src/components/usermanagement/ManagerModal/ManagerModal.tsx
+++ src/components/usermanagement/ManagerModal/ManagerModal.tsx
@@ -104,7 +104,7 @@ const ManagerModal = (props: ManagerModalProps) => {
       ...res
     };
     selectedCalabrioTeams.push(temp);
-    console.log("GCX selectedCalabrioTeams",temp, selectedCalabrioTeams);
+    console.log("GCX selectedCalabrioTeams", res, temp, selectedCalabrioTeams);
     setSelectedCalabrioTeams(selectedCalabrioTeams.map((team:any) => team.value));
   };
 
