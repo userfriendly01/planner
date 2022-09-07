7fdea0bc73e2c189a4a43d51f33d94319164ce79
Cuneo <Faith.Cuneo@LibertyMutual.com>
2022-09-07 10:11:41 -0400

    trying the mask without input ref

--- src/components/core/ModalPhoneNumber/ModalPhoneNumber.jsx
+++ src/components/core/ModalPhoneNumber/ModalPhoneNumber.jsx
@@ -32,7 +32,7 @@ const SwitchContainer = styled.div`
 
 const SevenDigitInputMask = inputProps => {
   const {
-    inputRef,
+    // inputRef,
     ...other
   } = inputProps;
   return (
@@ -41,9 +41,9 @@ const SevenDigitInputMask = inputProps => {
       guide={false}
       mask={sevenDigitMask}
       placeholderChar={"\u2000"}
-      ref={ref => {
-        inputRef(ref ? ref.inputElement : null);
-      }}
+      // ref={ref => {
+      //   inputRef(ref ? ref.inputElement : null);
+      // }}
       showMask />
   );
 };
@@ -51,7 +51,7 @@ const SevenDigitInputMask = inputProps => {
 
 const TenDigitInputMask = inputProps => {
   const {
-    inputRef,
+    // inputRef,
     ...other
   } = inputProps;
   return (
@@ -60,9 +60,9 @@ const TenDigitInputMask = inputProps => {
       guide={false}
       mask={tenDigitMask}
       placeholderChar={"\u2000"}
-      ref={ref => {
-        inputRef(ref ? ref.inputElement : null);
-      }}
+      // ref={ref => {
+      //   inputRef(ref ? ref.inputElement : null);
+      // }}
       showMask />
   );
 };
