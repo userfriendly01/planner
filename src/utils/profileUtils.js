4248be9e6f594717cd4b51792855d20715fdf23c
Matthew Creta <matthew.creta@libertymutual.com>
2022-10-06 20:48:46 -0400

    lint

--- src/utils/profileUtils.js
+++ src/utils/profileUtils.js
@@ -1,19 +1,20 @@
+import React from "react";
 import { Check } from "@mui/icons-material";
 
-export const formatProfileBooleanData = (number) => {
-    if (number === 1) {
-       return <Check />;
-    }
-    return '';
+export const formatProfileBooleanData = number => {
+  if (number === 1) {
+    return <Check />;
+  }
+  return "";
 };
 
 export const profileSettingsViews = [
-    {
-        value: "PROFILE_DIRECTORY",
-        label: "Profile Directory"
-    },
-    {
-        value: "PROFILE_SETTINGS",
-        label: "Profile Settings"
-    }
+  {
+    value: "PROFILE_DIRECTORY",
+    label: "Profile Directory"
+  },
+  {
+    value: "PROFILE_SETTINGS",
+    label: "Profile Settings"
+  }
 ];
