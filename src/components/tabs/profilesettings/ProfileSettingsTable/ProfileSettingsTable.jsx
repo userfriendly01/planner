9fd6b1aba9ca97abef4e61e986575804697af86a
Matthew Creta <matthew.creta@libertymutual.com>
2022-10-06 21:24:33 -0400

    test check

--- src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
+++ src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
@@ -9,6 +9,7 @@ import {
   TableContainer
 } from "./ProfileSettingsTable.Styles";
 import { formatProfileBooleanData } from "utils";
+import { Check } from "@mui/icons-material";
 
 const ProfileSettingsTable = props => {
   const { profileList } = props;
@@ -40,43 +41,43 @@ const ProfileSettingsTable = props => {
                 return(
                   <CustomTableRow key={entry.profile_id} data-testid="table-row">
                     <CustomTableData>
-                      <TableText>{`${entry.profile_id}`}</TableText>
+                      <TableText>{entry.profile_id}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${entry.profile_nme}`}</TableText>
+                      <TableText>{entry.profile_nme}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.recorded_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.recorded_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.auto_answd_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.auto_answd_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.pmt_prcsg_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.pmt_prcsg_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.otbnd_recorded_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.otbnd_recorded_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.acw_option_i.data[0])}`}</TableText>
+                      <TableText><Check/></TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.manual_recorded_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.manual_recorded_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.acw_data_entry_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.acw_data_entry_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.manual_record_inbound_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.manual_record_inbound_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.agent_assisted_pay_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.agent_assisted_pay_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.completeTasksOnActivityChange_option_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.completeTasksOnActivityChange_option_i.data[0])}</TableText>
                     </CustomTableData>
                     <CustomTableData>
-                      <TableText>{`${formatProfileBooleanData(entry.policy_number_edit_i.data[0])}`}</TableText>
+                      <TableText>{formatProfileBooleanData(entry.policy_number_edit_i.data[0])}</TableText>
                     </CustomTableData>
                   </CustomTableRow>
                 );
