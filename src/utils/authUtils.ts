881038d9cdd5b4477852dffd5c4469931db364dc
Twinkle Katoch <twinkle.katoch@libertymutual.com>
2022-10-25 18:10:41 -0500

    Removing my n-number

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -16,8 +16,7 @@ export const checkIfPO = (nNumber: string): boolean => {
     "n0116796",
     "n0088625",
     "n0183277",
-    "n0196231",
-    "n1590719"
+    "n0196231"
   ];
 
   return productOwners.includes(nNumber.toLowerCase());
