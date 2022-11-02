59e24507ed50cf9044d56a96e4b5ff854d59b992
Twinkle Katoch <twinkle.katoch@libertymutual.com>
2022-11-02 13:36:01 -0500

    renaming

--- src/services/createProfile.ts
+++
@@ -1,7 +0,0 @@
-import {
-  apiPaths,
-  ProfilePayload
-} from "globals";
-import { myAxios } from "utils";
-
-export const createProfile = (profile: ProfilePayload): Promise<any> => myAxios.post(apiPaths.PROFILES, profile);
