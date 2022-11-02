0150f856b383c0b4aab40820ca3b5d9fe293eb05
Matthew Creta <matthew.creta@libertymutual.com>
2022-11-02 12:27:58 -0400

    add profile to function args

--- src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
+++ src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
@@ -32,7 +32,7 @@ const ProfileSettingsTable = props => {
   const { profileList, loggedInRep, setProfileModalState } = props;
   const setForm = profileEntryFormDispatch();
 
-  const editButtonOnClick = event => {
+  const editButtonOnClick = (profile) => event => {
     event.stopPropagation();
     setForm({
       type: profileEntryFormActions.SET_UPDATE_PROFILE_FORM_STATE,
@@ -122,7 +122,7 @@ const ProfileSettingsTable = props => {
                     {
                       checkIfPO(loggedInRep) ? 
                         <CustomTableData>
-                          <IconWrapper onClick={editButtonOnClick} data-testid="edit-button">
+                          <IconWrapper onClick={editButtonOnClick(profile)} data-testid="edit-button">
                             <Edit fontSize={"inherit"}/>
                           </IconWrapper>
                         </CustomTableData>
