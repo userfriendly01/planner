67052cecf08f63c34d73497765521ee85d44f38d
Rob Zaffino <rob.zaffino@libertymutual.com>
2022-11-18 13:44:41 -0500

    temporary hard-code of acw options; no API avail

--- src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
+++ src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
@@ -66,6 +66,7 @@ const ProfileSettingsTable = props => {
           <tbody>
             {
               profileList.sort(sortProfilesById).map(profile => {
+                {profile.acwOptions={ display_nme: "Call Type", options_id: 1, profile_id: 15, row_crtn_dtm: "2019-10-24T12:58:48.000Z", row_updt_dtm:"2019-10-24T12:58:48.000Z", wrkr_tsk_info_id:1}}
                 return(
                   <CustomTableRow key={profile.profile_id} data-testid="table-row">
                     <CustomTableData>
