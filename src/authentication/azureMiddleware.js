1acfa5fc8e93eb489f2abbb05fc5ed28f14adfd8
Cuneo <Faith.Cuneo@LibertyMutual.com>
2022-11-30 18:22:38 -0500

    updating the reply url to to local host address

--- src/authentication/azureMiddleware.js
+++ src/authentication/azureMiddleware.js
@@ -1,7 +1,8 @@
 import { myAxios } from "utils";
 import {
   authenticateUser,
-  configureAxiosForPKCE
+  configureAxiosForPKCE,
+  IDP
 } from "@lmig/pkce-authentication-utilities";
 
 configureAxiosForPKCE(myAxios);
@@ -16,9 +17,8 @@ const renderAuthFailed = () => {
 
 const isSuccessfulAuthentication = authenticateUser({
   clientId: "cc2d6284-c9c4-43fe-9164-4ee0cb8fac50",
-  authUrl: "https://login.windows.net/08a83339-90e7-49bf-9075-957ccd561bf1/oauth2/v2.0/authorize",
-  redirectUrl: "https://cicct-softphone-admin-ui-development.us-east-1.np.paas.lmig.com/azure/auth-callback",
-  tokenUrl: "https://login.windows.net/08a83339-90e7-49bf-9075-957ccd561bf1/oauth2/v2.0/token",
+  idp: IDP.AZURE,
+  redirectUrl: "https://localhost:8443/auth-callback",
   axios: myAxios,
   renderAuthInProgress: renderAuthInProgress,
   renderAuthFailed: renderAuthFailed
