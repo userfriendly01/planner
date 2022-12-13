5be0fdb34ef6cc8ab0baed29010123def6670085
Matthew Creta <matthew.creta@libertymutual.com>
2022-12-13 17:58:17 -0500

    add insrert id

--- src/components/tabs/profilesettings/ProfileEntryForm/ProfileFormButtons.tsx
+++ src/components/tabs/profilesettings/ProfileEntryForm/ProfileFormButtons.tsx
@@ -70,7 +70,7 @@ const ProfileFormButtons = (props: ProfileFormButtonsProps) => {
     createProfile(payload).then(response => {
       updateLoading({
         ...loading,
-        overlayMessage: `Successfully added new profile with ID ${JSON.stringify(response.data)}`,
+        overlayMessage: `Successfully added new profile with ID ${response.data?.profile.insertId}`,
         saveStatus: ModalOverlayStatuses.SUCCESS,
         saveProfile: true
       });
