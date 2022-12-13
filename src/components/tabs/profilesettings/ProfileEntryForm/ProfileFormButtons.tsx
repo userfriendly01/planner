1ef3e16f6e5e80d1456091aebe815a32710fa93d
Matthew Creta <matthew.creta@libertymutual.com>
2022-12-13 17:49:16 -0500

    fix log

--- src/components/tabs/profilesettings/ProfileEntryForm/ProfileFormButtons.tsx
+++ src/components/tabs/profilesettings/ProfileEntryForm/ProfileFormButtons.tsx
@@ -70,7 +70,7 @@ const ProfileFormButtons = (props: ProfileFormButtonsProps) => {
     createProfile(payload).then(response => {
       updateLoading({
         ...loading,
-        overlayMessage: `Successfully added new profile with ID ${response.data}`,
+        overlayMessage: `Successfully added new profile with ID ${JSON.stringify(response.data)}`,
         saveStatus: ModalOverlayStatuses.SUCCESS,
         saveProfile: true
       });
