dba4dd4c965e461e12b4def587e1c3d090a02be2
Rob Zaffino <rob.zaffino@libertymutual.com>
2022-12-13 14:09:16 -0500

    log profile.activities

--- src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
+++ src/components/tabs/profilesettings/ProfileSettingsTable/ProfileSettingsTable.jsx
@@ -156,6 +156,7 @@ const ProfileSettingsTable = props => {
                     </CustomTableData>
                     <CustomTableData>
                       <TableDataFlex>
+                        {console.log("rz profile.activities=", profile.activities)}
                         {
                           JSON.parse(profile.activities).map(activity => {
                             return <div key={`${activity.name}`}>{formatActivityData(activity.name)}</div>;
