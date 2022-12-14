fd51858e2eca04a4bca358e9858c70e5d33d8f33
Matthew Creta <matthew.creta@libertymutual.com>
2022-12-14 11:52:24 -0500

    fix acticity parse

--- src/context/test/profileEntryFormReducer.test.js
+++ src/context/test/profileEntryFormReducer.test.js
@@ -229,7 +229,7 @@ describe("profileEntryFormReducer", () => {
           type: "Buffer",
           data: [1]
         },
-        activities: "[{\"id\": 1, \"name\": \"Offline\", \"availability\": 0}]"
+        activities: [{id: 1, name: "Offline", availability: 0}]
       };
 
       const expectedState = {
