4d9272e638067f2b1afc976acd8c23760fc0d608
Matthew Creta <matthew.creta@libertymutual.com>
2022-12-19 12:17:12 -0500

    cleanup

--- src/services/test/workerTaskInfo.test.js
+++ src/services/test/workerTaskInfo.test.js
@@ -33,28 +33,3 @@ describe("getProfileWorkerTaskInfo", () => {
     });
   });
 });
-
-describe("getWorkerTaskInfo", () => {
-  describe("call succeeds", () => {
-    const data = { huzzah: "you are winner" };
-    beforeEach(() => axiosMock.onGet(workerTaskInfoEndpoint).replyOnce(200, data));
-    test("should resolve with any successful response", done => {
-      getWorkerTaskInfo()
-        .then(resolvedValue => {
-          expect(resolvedValue).toEqual(data);
-          done();
-        });
-    });
-  });
-  // rz add test back in
-  // describe("call fails", () => {
-  //   const badResponse = { wahh: "boo" };
-  //   beforeEach(() => axiosMock.onGet(workerTaskInfoEndpoint).replyOnce(500, badResponse));
-  //   test("should reject with error", done => {
-  //     getWorkerTaskInfo().catch(rejectedVal => {
-  //       expect(rejectedVal).toEqual(new Error("Request failed with status code 500"));
-  //       done();
-  //     });
-  //   });
-  // });
-});
