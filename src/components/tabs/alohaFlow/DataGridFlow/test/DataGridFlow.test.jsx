38c986cb179964b118667733f38ad59fdd09072d
Eric Phetteplace <eric.phetteplace@libertymutual.com>
2023-01-04 11:13:24 -0500

    CECFR-1117 implement mock for retrieveFlowData
    It's different than the other mocks.  At least this removed the
    'undefined' error for
    ```expect(retrieveFlowData).toBe...```

--- src/components/tabs/alohaFlow/DataGridFlow/test/DataGridFlow.test.jsx
+++ src/components/tabs/alohaFlow/DataGridFlow/test/DataGridFlow.test.jsx
@@ -12,7 +12,7 @@ import {
 import { CustomToast } from "../../../../core/index";
 import DataGridFlow from "../DataGridFlow";
 import React from "react";
-//import { retrieveFlowData } from "services";
+import { retrieveFlowData } from "services";
 
 jest.mock("../../../../core/index", () => {
   const originalModule = jest.requireActual("../../../../core/index");
@@ -41,6 +41,7 @@ jest.mock("utils", () => {
     )
   };
 });
+
 jest.mock("../../CustomActions", () => {
   const originalModule = jest.requireActual("../../CustomActions");
 
@@ -54,31 +55,31 @@ jest.mock("../../CustomActions", () => {
   };
 });
 
-// const services = jest.createMockFromModule("services");
-// // eslint-disable-next-line no-unused-vars
-// services.retrieveFlowData = jest.fn((_a,_b) => { Promise.resolve(
-//   [
-//     {
-//       id: 1,
-//       pkey: "+18005551212",
-//       agentId: "agent1",
-//       brand: "brand1",
-//       callFlowTemplate: "cft1",
-//       channel: "channel1",
-//       content: {
-//         callerType: "Customer",
-//         callFlowRoute: "routre A1",
-//         dataRequests: ["Classify"],
-//         greetingMessages: "Hello and welcome!",
-//         transferNumber: "+12223334444"
-//       },
-//       createTime: "2020-01-01T15:14:13.000Z",
-//       dialedDescription: "Test case",
-//       employeeId: "n1234455",
-//       userDestination: "Avaya"
-//     }
-//   ]);
-// });
+const services = jest.createMockFromModule("services");
+//eslint-disable-next-line no-unused-vars
+services.retrieveFlowData = jest.fn((_a,_b) => { Promise.resolve(
+  [
+    {
+      id: 1,
+      pkey: "+18005551212",
+      agentId: "agent1",
+      brand: "brand1",
+      callFlowTemplate: "cft1",
+      channel: "channel1",
+      content: {
+        callerType: "Customer",
+        callFlowRoute: "routre A1",
+        dataRequests: ["Classify"],
+        greetingMessages: "Hello and welcome!",
+        transferNumber: "+12223334444"
+      },
+      createTime: "2020-01-01T15:14:13.000Z",
+      dialedDescription: "Test case",
+      employeeId: "n1234455",
+      userDestination: "Avaya"
+    }
+  ]);
+});
 
 
 const renderComponent = () => render(
@@ -95,7 +96,6 @@ describe("<DataGridFlow />", () => {
     expect(EditFlow).toBeCalledTimes(0);
     expect(getAccessToken).toBeCalledTimes(2);
     expect(getValidSkillsObject).toBeCalledTimes(0);
-    // expect(retrieveFlowData).toBeCalledTimes(0);
-
+    expect(retrieveFlowData).toBeCalledTimes(0);
   });
 });
