ff78f6c405d476699843c2da971d13449beda1c8
Cuneo <Faith.Cuneo@LibertyMutual.com>
2023-01-20 16:15:23 -0500

    ignore case = false

---
+++ src/components/tabs/usermanagement/OnboardNewUser/Skills/PriorityDropdown/test/PriorityDropDown.test.jsx
@@ -0,0 +1,59 @@
+import PriorityDropDown from "../PriorityDropDown";
+import { Dropdown } from "components";
+import React from "react";
+import {
+  expectMockedComponent,
+  expectOnlyPassedProps,
+  getMockedComponentProps,
+  render,
+  setupMockedComponents
+} from "testUtils";
+
+jest.mock("components", () => ({
+  __esModule: true,
+  Dropdown: jest.fn()
+}));
+
+describe("<PriorityDropDown />", () => {
+  const priorities = [1, 2, 3];
+  const mockUpdatePriority = jest.fn();
+  const renderComponent = () => {
+    return render(<PriorityDropDown
+      availablePriorities={priorities}
+      priorityValue={1}
+      updatePriority={mockUpdatePriority}
+    />);
+  };
+  beforeEach(() => {
+    setupMockedComponents({ Dropdown });
+    mockUpdatePriority.mockClear();
+  });
+  describe("initial state", () => {
+    test("should render Dropdown with correct props", () => {
+      const rendered = renderComponent();
+      expect(Dropdown.mock.calls.length).toBe(1);
+      expectMockedComponent(rendered, { Dropdown });
+      expectOnlyPassedProps(Dropdown, {
+        options: [{
+          label: "1",
+          value: 1
+        },
+        {
+          label: "2",
+          value: 2
+        },
+        {
+          label: "3",
+          value: 3
+        }],
+        value: {
+          label: "1",
+          value: 1
+        }
+      });
+      const props = getMockedComponentProps(Dropdown);
+      props.updateValue(null, { value: "cool" });
+      expect(mockUpdatePriority).toHaveBeenCalledWith("cool");
+    });
+  });
+});
