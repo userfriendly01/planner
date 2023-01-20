77e2be7d3a436f030129a0f2770eb7496d6bc427
Cuneo <Faith.Cuneo@LibertyMutual.com>
2023-01-20 17:50:18 -0500

    I am so late for dinner

--- src/components/tabs/usermanagement/OnboardNewUser/Skills/index.js
+++ src/components/tabs/usermanagement/OnboardNewUser/Skills/index.js
@@ -1,4 +1,4 @@
 export * from "./DefaultSkillSelector";
 export * from "./SkillDropDown";
-export { default as PriorityDropDownButts } from "./PriorityDropDownButts/PriorityDropDownButts";
+export { default as PriorityDropDownButts } from "./PriorityDropdownButts/PriorityDropdownButts";
 export { default as SkillsFormInfo } from "./SkillsFormInfo/SkillsFormInfo";
