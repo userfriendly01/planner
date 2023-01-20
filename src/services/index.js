654f7deadc1ddba394d963dbe45c0c1619b27e3b
Matthew Creta <matthew.creta@libertymutual.com>
2023-01-20 14:42:24 -0500

    fix linting add tests

--- src/services/index.js
+++ src/services/index.js
@@ -11,4 +11,4 @@ export * from "./tfnActivation";
 export * from "./updateUser";
 export * from "./profile";
 export * from "./callTags";
-export * from "./aggregateQueues"
+export * from "./aggregateQueues";
