d470dc48abf65572facf898a2b5ba4b443c2b5f2
Mrinal Denre <mrinal.denre@libertymutual.com>
2023-01-24 23:06:36 +0530

    Fix failed test cases

--- src/components/tabs/usermanagement/TritonUsersView/ManagerModal/test/ManagerModal.test.jsx
+++ src/components/tabs/usermanagement/TritonUsersView/ManagerModal/test/ManagerModal.test.jsx
@@ -51,6 +51,10 @@ jest.mock("@mui/material", () => ({
   Checkbox: jest.fn()
 }));
 
+jest.mock("@mui/x-date-pickers/TimePicker", () => ({
+  TimePicker: jest.fn()
+}));
+
 jest.mock("services", () => ({
   FetchUserResponse: jest.requireActual("services").FetchUserResponse,
   addManager: jest.fn(),
