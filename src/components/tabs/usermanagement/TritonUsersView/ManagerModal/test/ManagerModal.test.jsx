1aec14105b715f439f74138bbd1e173b721ab977
Mrinal Denre <mrinal.denre@libertymutual.com>
2023-01-24 23:14:46 +0530

    Resove faild test cases

--- src/components/tabs/usermanagement/TritonUsersView/ManagerModal/test/ManagerModal.test.jsx
+++ src/components/tabs/usermanagement/TritonUsersView/ManagerModal/test/ManagerModal.test.jsx
@@ -51,6 +51,13 @@ jest.mock("@mui/material", () => ({
   Checkbox: jest.fn()
 }));
 
+jest.mock("@mui/x-data-grid",()=>({
+  __esModule: true,
+  DataGrid: jest.fn(),
+  GridToolbar: jest.fn(),
+  GridRenderCellParams: jest.fn()
+}));
+
 jest.mock("@mui/x-date-pickers/TimePicker", () => ({
   TimePicker: jest.fn()
 }));
