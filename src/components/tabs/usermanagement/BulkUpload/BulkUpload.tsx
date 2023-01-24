4a8d28a8f83d3476540a91fab03c590695fd8985
Cuneo <Faith.Cuneo@LibertyMutual.com>
2023-01-23 23:18:43 -0500

    pulled more out

--- src/components/tabs/usermanagement/BulkUpload/BulkUpload.tsx
+++ src/components/tabs/usermanagement/BulkUpload/BulkUpload.tsx
@@ -68,16 +68,17 @@ const BulkUpload = () => {
   const readUploadFile = (e: any) => {
     e.preventDefault();
     if (e.target.files) {
-      const reader = new FileReader();
-      reader.onload = e => {
-        const data = e.target.result;
-        const workbook = xlsx.read(data, { type: "array" });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const json = xlsx.utils.sheet_to_json(worksheet);
-        console.log(json);
-      };
-      reader.readAsArrayBuffer(e.target.files[0]);
+      console.log("EEEE", e);
+      // const reader = new FileReader();
+      // reader.onload = e => {
+      //   const data = e.target.result;
+      //   const workbook = xlsx.read(data, { type: "array" });
+      //   const sheetName = workbook.SheetNames[0];
+      //   const worksheet = workbook.Sheets[sheetName];
+      //   const json = xlsx.utils.sheet_to_json(worksheet);
+      //   console.log(json);
+      // };
+      // reader.readAsArrayBuffer(e.target.files[0]);
     }
   };
 
@@ -132,7 +133,7 @@ const BulkUpload = () => {
           name="upload"
           id="upload"
           onChange={readUploadFile}
-        >Import CSV</input>
+        />
       </ButtonWrapper>
     </BulkChangesWrapper>
   );
