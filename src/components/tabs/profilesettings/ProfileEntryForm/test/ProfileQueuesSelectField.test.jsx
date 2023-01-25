e3df14a65b6b7f3236efb6365607315d013fe856
Matthew Creta <matthew.creta@libertymutual.com>
2023-01-25 09:44:01 -0500

    finish tests

--- src/components/tabs/profilesettings/ProfileEntryForm/test/ProfileQueuesSelectField.test.jsx
+++ src/components/tabs/profilesettings/ProfileEntryForm/test/ProfileQueuesSelectField.test.jsx
@@ -6,16 +6,22 @@ import {
 import { Dropdown } from "components";
 import { useAdminState } from "context";
 import React from "react";
+import MockAdapter from "axios-mock-adapter";
+import { myAxios } from "utils";
 import {
   expectMockedComponent,
   render,
   setupMockedComponents,
   fireEvent,
   getMockedComponentProps,
+  mockAggregateQueues,
   initialTestState,
   skillsList
 } from "testUtils";
-import { theme } from "globals";
+import {
+  theme,
+  apiPaths
+} from "globals";
 import { ThemeProvider } from "styled-components";
 import { act } from "react-dom/test-utils";
 import { getAggregateQueuesType } from "services";
@@ -46,17 +52,11 @@ const renderComponent = mockTransferQueues => render(
   </ThemeProvider>
 );
 
-getAggregateQueuesType.mockImplementation(() => {
-  return Promise.resolve([{
-    aggregate_queues_id: 1,
-    aggregate_queues_nme: 'Licensed Sales Center',
-    aggregate_queues_type: 'aggregate',
-    owner_type: 'profile',
-    worker_sid: null,
-    row_crtn_dtm: '',
-    row_updt_dtm: null
-  }]);
-});
+getAggregateQueuesType.mockImplementation(() => { return Promise.resolve(200, { response: "success" } ); });
+
+const statusCode = 500;
+const axiosMock = new MockAdapter(myAxios);
+const aggregateQueueTypeEndpoint = apiPaths.GET_AGGREGATE_QUEUES_TYPE;
 
 const getAddTransferQueuesButton = rendered => rendered.getByTestId("add-queue-button");
 const getDeleteTransferQueuesButton = (rendered, instance) => rendered.getAllByTestId("delete-queue-button")[instance];
@@ -70,6 +70,7 @@ describe("<ProfileQueuesSelectField />", () => {
       Delete,
       Dropdown
     });
+    axiosMock.onGet(aggregateQueueTypeEndpoint).reply(200, mockAggregateQueues);
     mockSetQueueList.mockClear();
   });
 
@@ -82,171 +83,181 @@ describe("<ProfileQueuesSelectField />", () => {
     });
   });
 
-  // describe("changes made to the add transfer queue drop down", () => {
-  //   beforeEach(() => {
-  //     const mockNewSkill = [{
-  //       name: "lscOBDialer1",
-  //       ctmSkillId: 1,
-  //       ctmSkillDisplayName: "lsc OB Dialer 1",
-  //       profiles: [{
-  //         profileName: "Licensed Sales Center",
-  //         profileId: 32
-  //       }],
-  //       flashMessage: "",
-  //       closedMessage: "",
-  //       levels: [ 1, 2, 3],
-  //       timeOfDays: [],
-  //       vhCallTarget: null,
-  //       vhCallerId: null,
-  //       vhThreshold: null
-  //     }];
-  //     React.useState = jest.fn()
-  //       .mockReturnValueOnce([mockNewSkill, jest.fn()])
-  //       .mockReturnValueOnce([skillsList, jest.fn()]);
-  //   });
+  describe(aggregateQueueTypeEndpoint, () => {
+    describe("aggregateQueueTypeEndpoint service call returned an error", () => {
+      beforeEach(() => {
+        axiosMock.onGet(aggregateQueueTypeEndpoint).reply(statusCode, { fail: "oh the horror" });
+      });
+      test("should return 'An error occurred while logging in.'", async () => {
+        try {
+          renderComponent([]);
+        } catch(err) {
+          expect(err.msg).toBe("Failed to fetch callTags from service");
+        }
+      });
+    });
+  });
 
-  //   test("should render transfer queue drop down with correct options", () => {
-  //     const rendered = renderComponent([{
-  //       name: "aisgL1",
-  //       ctmSkillId: 2,
-  //       ctmSkillDisplayName: "aisg L1",
-  //       profiles: [{
-  //         profileName: "AISG",
-  //         profileId: 4
-  //       }],
-  //       flashMessage: "",
-  //       closedMessage: "Sorry, we're closed.",
-  //       levels: [],
-  //       timeOfDays: [],
-  //       vhCallTarget: null,
-  //       vhCallerId: null,
-  //       vhThreshold: null
-  //     }]);
-  //     expect(rendered.container).not.toHaveTextContent("lsc OB Dialer 1");
-  //     act(() => {
-  //       const { updateValue } = getMockedComponentProps(Dropdown);
-  //       updateValue("", [{ value: 1 }] );
-  //     });
-  //     act(() => {
-  //       fireEvent.click(getAddTransferQueuesButton(rendered));
-  //     });
-  //     expect(mockSetQueueList).toHaveBeenCalledWith([
-  //       {
-  //         name: "aisgL1",
-  //         ctmSkillId: 2,
-  //         ctmSkillDisplayName: "aisg L1",
-  //         profiles: [{
-  //           profileName: "AISG",
-  //           profileId: 4
-  //         }],
-  //         flashMessage: "",
-  //         closedMessage: "Sorry, we're closed.",
-  //         levels: [],
-  //         timeOfDays: [],
-  //         vhCallTarget: null,
-  //         vhCallerId: null,
-  //         vhThreshold: null
-  //       },
-  //       {
-  //         name: "lscOBDialer1",
-  //         ctmSkillId: 1,
-  //         ctmSkillDisplayName: "lsc OB Dialer 1",
-  //         profiles: [{
-  //           profileName: "Licensed Sales Center",
-  //           profileId: 32
-  //         }],
-  //         flashMessage: "",
-  //         closedMessage: "",
-  //         levels: [ 1, 2, 3],
-  //         timeOfDays: [],
-  //         vhCallTarget: null,
-  //         vhCallerId: null,
-  //         vhThreshold: null
-  //       }
-  //     ]);
-  //   });
-  // });
+  describe("changes made to the add transfer queue drop down", () => {
+    beforeEach(() => {
+      const mockNewSkill = [{
+        name: "lscOBDialer1",
+        ctmSkillId: 1,
+        ctmSkillDisplayName: "lsc OB Dialer 1",
+        profiles: [{
+          profileName: "Licensed Sales Center",
+          profileId: 32
+        }],
+        flashMessage: "",
+        closedMessage: "",
+        levels: [ 1, 2, 3],
+        timeOfDays: [],
+        vhCallTarget: null,
+        vhCallerId: null,
+        vhThreshold: null
+      }];
+      React.useState = jest.fn()
+        .mockReturnValueOnce([mockAggregateQueues, jest.fn()])
+        .mockReturnValueOnce([mockNewSkill, jest.fn()])
+        .mockReturnValueOnce([skillsList, jest.fn()]);
+    });
 
-  // describe("remove button", () => {
-  //   beforeEach(() => {
-  //     const mockNewSkill = [{
-  //       name: "lscOBDialer1",
-  //       ctmSkillId: 1,
-  //       ctmSkillDisplayName: "lsc OB Dialer 1",
-  //       profiles: [{
-  //         profileName: "Licensed Sales Center",
-  //         profileId: 32
-  //       }],
-  //       flashMessage: "",
-  //       closedMessage: "",
-  //       levels: [ 1, 2, 3],
-  //       timeOfDays: [],
-  //       vhCallTarget: null,
-  //       vhCallerId: null,
-  //       vhThreshold: null
-  //     }];
-  //     React.useState = jest.fn()
-  //       .mockReturnValueOnce([mockNewSkill, jest.fn()])
-  //       .mockReturnValueOnce([skillsList, jest.fn()]);
-  //   });
+    test("should render transfer queue drop down with correct options", () => {
+      const rendered = renderComponent([{
+        name: "aisgL1",
+        ctmSkillId: 2,
+        ctmSkillDisplayName: "aisg L1",
+        profiles: [{
+          profileName: "AISG",
+          profileId: 4
+        }],
+        flashMessage: "",
+        closedMessage: "Sorry, we're closed.",
+        levels: [],
+        timeOfDays: [],
+        vhCallTarget: null,
+        vhCallerId: null,
+        vhThreshold: null
+      }]);
+      expect(rendered.container).not.toHaveTextContent("lsc OB Dialer 1");
+      act(() => {
+        const { updateValue } = getMockedComponentProps(Dropdown);
+        updateValue("", [{ value: 1 }] );
+      });
+      act(() => {
+        fireEvent.click(getAddTransferQueuesButton(rendered));
+      });
+      expect(mockSetQueueList).toHaveBeenCalledWith([
+        {
+          name: "aisgL1",
+          ctmSkillId: 2,
+          ctmSkillDisplayName: "aisg L1",
+          profiles: [{
+            profileName: "AISG",
+            profileId: 4
+          }],
+          flashMessage: "",
+          closedMessage: "Sorry, we're closed.",
+          levels: [],
+          timeOfDays: [],
+          vhCallTarget: null,
+          vhCallerId: null,
+          vhThreshold: null
+        },
+        {
+          aggregate_queues_id: 4,
+          aggregate_queues_nme: 'Licensed Sales Center',
+          aggregate_queues_type: 'aggregate',
+          owner_type: 'profile',
+          worker_sid: null,
+          row_crtn_dtm: '',
+          row_updt_dtm: null
+        }
+      ]);
+    });
+  });
 
-  //   test("should display once for each transfer queue; when clicked, queue should be removed", () => {
-  //     const rendered = renderComponent([
-  //       {
-  //         name: "aisgL1",
-  //         ctmSkillId: 2,
-  //         ctmSkillDisplayName: "aisg L1",
-  //         profiles: [{
-  //           profileName: "AISG",
-  //           profileId: 4
-  //         }],
-  //         flashMessage: "",
-  //         closedMessage: "Sorry, we're closed.",
-  //         levels: [],
-  //         timeOfDays: [],
-  //         vhCallTarget: null,
-  //         vhCallerId: null,
-  //         vhThreshold: null
-  //       },
-  //       {
-  //         name: "lscOBDialer1",
-  //         ctmSkillId: 1,
-  //         ctmSkillDisplayName: "lsc OB Dialer 1",
-  //         profiles: [{
-  //           profileName: "Licensed Sales Center",
-  //           profileId: 32
-  //         }],
-  //         flashMessage: "",
-  //         closedMessage: "",
-  //         levels: [ 1, 2, 3],
-  //         timeOfDays: [],
-  //         vhCallTarget: null,
-  //         vhCallerId: null,
-  //         vhThreshold: null
-  //       }
-  //     ]);
-  //     expectMockedComponent(rendered, { Delete }, 2);
-  //     act(() => {
-  //       fireEvent.click(getDeleteTransferQueuesButton(rendered, 1));
-  //     });
-  //     expect(mockSetQueueList).toHaveBeenCalledWith([
-  //       {
-  //         name: "aisgL1",
-  //         ctmSkillId: 2,
-  //         ctmSkillDisplayName: "aisg L1",
-  //         profiles: [{
-  //           profileName: "AISG",
-  //           profileId: 4
-  //         }],
-  //         flashMessage: "",
-  //         closedMessage: "Sorry, we're closed.",
-  //         levels: [],
-  //         timeOfDays: [],
-  //         vhCallTarget: null,
-  //         vhCallerId: null,
-  //         vhThreshold: null
-  //       }
-  //     ]);
-  //   });
-  // });
+  describe("remove button", () => {
+    beforeEach(() => {
+      const mockNewSkill = [{
+        name: "lscOBDialer1",
+        ctmSkillId: 1,
+        ctmSkillDisplayName: "lsc OB Dialer 1",
+        profiles: [{
+          profileName: "Licensed Sales Center",
+          profileId: 32
+        }],
+        flashMessage: "",
+        closedMessage: "",
+        levels: [ 1, 2, 3],
+        timeOfDays: [],
+        vhCallTarget: null,
+        vhCallerId: null,
+        vhThreshold: null
+      }];
+      React.useState = jest.fn()
+        .mockReturnValueOnce([mockAggregateQueues, jest.fn()])
+        .mockReturnValueOnce([mockNewSkill, jest.fn()])
+        .mockReturnValueOnce([skillsList, jest.fn()]);
+    });
+
+    test("should display once for each transfer queue; when clicked, queue should be removed", () => {
+      const rendered = renderComponent([
+        {
+          name: "aisgL1",
+          ctmSkillId: 2,
+          ctmSkillDisplayName: "aisg L1",
+          profiles: [{
+            profileName: "AISG",
+            profileId: 4
+          }],
+          flashMessage: "",
+          closedMessage: "Sorry, we're closed.",
+          levels: [],
+          timeOfDays: [],
+          vhCallTarget: null,
+          vhCallerId: null,
+          vhThreshold: null
+        },
+        {
+          name: "lscOBDialer1",
+          ctmSkillId: 1,
+          ctmSkillDisplayName: "lsc OB Dialer 1",
+          profiles: [{
+            profileName: "Licensed Sales Center",
+            profileId: 32
+          }],
+          flashMessage: "",
+          closedMessage: "",
+          levels: [ 1, 2, 3],
+          timeOfDays: [],
+          vhCallTarget: null,
+          vhCallerId: null,
+          vhThreshold: null
+        }
+      ]);
+      expectMockedComponent(rendered, { Delete }, 2);
+      act(() => {
+        fireEvent.click(getDeleteTransferQueuesButton(rendered, 1));
+      });
+      expect(mockSetQueueList).toHaveBeenCalledWith([
+        {
+          name: "aisgL1",
+          ctmSkillId: 2,
+          ctmSkillDisplayName: "aisg L1",
+          profiles: [{
+            profileName: "AISG",
+            profileId: 4
+          }],
+          flashMessage: "",
+          closedMessage: "Sorry, we're closed.",
+          levels: [],
+          timeOfDays: [],
+          vhCallTarget: null,
+          vhCallerId: null,
+          vhThreshold: null
+        }
+      ]);
+    });
+  });
 });
