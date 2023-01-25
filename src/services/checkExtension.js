40d6b32467fe68ccc2e4912e49b9a89d1ce61389
Cuneo <Faith.Cuneo@LibertyMutual.com>
2023-01-25 14:23:49 -0500

    commented out extension stuff

--- src/services/checkExtension.js
+++ src/services/checkExtension.js
@@ -1,5 +1,5 @@
 import { apiPaths } from "globals";
-import { SearchParams } from "../ExtensionSearchParams";
+// import { SearchParams } from "../ExtensionSearchParams";
 import { myAxios } from "utils";
 
 export const checkExtension = extension => myAxios.post(apiPaths.CHECK_EXTENSION, { extension })
@@ -7,32 +7,32 @@ export const checkExtension = extension => myAxios.post(apiPaths.CHECK_EXTENSION
     return res.data.isValid !== undefined ? res.data.isValid : false;
   });
 
-const pickANumber = () => {
-  const searchParams = SearchParams.getValues();
-  let extNum = null;
-  while (!extNum) {
-    const oneNum = searchParams.MinExtensionNum + Math.floor((Math.random() * searchParams.ExtensionNumRange));
-    if (searchParams.ReservedExtensions.indexOf(oneNum) === -1) {
-      extNum = oneNum.toString();
-    }
-  }
-  return extNum;
-};
+// const pickANumber = () => {
+//   const searchParams = SearchParams.getValues();
+//   let extNum = null;
+//   while (!extNum) {
+//     const oneNum = searchParams.MinExtensionNum + Math.floor((Math.random() * searchParams.ExtensionNumRange));
+//     if (searchParams.ReservedExtensions.indexOf(oneNum) === -1) {
+//       extNum = oneNum.toString();
+//     }
+//   }
+//   return extNum;
+// };
 
-export const generateExtension = () => {
-  const maxAttempts = 5;
+// export const generateExtension = () => {
+//   const maxAttempts = 5;
 
-  const validateGeneratedNumber = async attemptNumber => {
-    const extension = pickANumber();
-    const validExtension = await checkExtension(extension);
-    if(validExtension){
-      return Promise.resolve(extension);
-    } else if(attemptNumber === maxAttempts) {
-      return Promise.reject("Enable to generate extension");
-    } else {
-      return validateGeneratedNumber(attemptNumber + 1 );
-    }
-  };
+//   const validateGeneratedNumber = async attemptNumber => {
+//     const extension = pickANumber();
+//     const validExtension = await checkExtension(extension);
+//     if(validExtension){
+//       return Promise.resolve(extension);
+//     } else if(attemptNumber === maxAttempts) {
+//       return Promise.reject("Enable to generate extension");
+//     } else {
+//       return validateGeneratedNumber(attemptNumber + 1 );
+//     }
+//   };
 
-  validateGeneratedNumber(1);
-};
+//   validateGeneratedNumber(1);
+// };
