016334f70b54ffc66bfebf85004d73c008114962
Matthew Creta <matthew.creta@libertymutual.com>
2023-01-25 13:11:44 -0500

    remove webopack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const ESLintPlugin = require("eslint-webpack-plugin");
-const { DefinePlugin } = require("webpack");
 const path = require("path");
 
 const resolvePathInSrc = resourceInSrc => {
@@ -17,9 +16,6 @@ const config = {
   },
   plugins: [
     new ESLintPlugin({ failOnWarning: true }),
-    new DefinePlugin({
-      "process.env.APP_ENV": JSON.stringify(process.env.APP_ENV) || JSON.stringify("youNeedToSetThisLocally"),
-    })
   ],
   module: {
     rules: [
