969bed249d453ed397092acf26ee6083486d4b70
Mrinal Denre <mrinal.denre@libertymutual.com>
2023-02-01 17:55:08 +0530

    Add New Routing Attributes and Create Test Cases for GridMasterRouting

--- src/components/tabs/alohaRouting/DataGridRouting/GridMaster.tsx
+++ src/components/tabs/alohaRouting/DataGridRouting/GridMaster.tsx
@@ -16,7 +16,7 @@ const getGridMasterData = (data: CctSharedCallRoutingDb[]): RoutingMasterData =>
       return JSON.parse(localStorageMasterData) as RoutingMasterData;
     } else {
       const masterData: Record<string, any> = {};
-      data.forEach((elem: Record<string, any>) => masterDataItems.forEach((key: string) => (masterData[key] ? masterData[key].push(elem[key]) : masterData[key] = [masterData[key]])));
+      data.forEach((elem: Record<string, any>) => masterDataItems.forEach((key: string) => (masterData[key] ? masterData[key].push(elem[key]) : masterData[key] = [elem[key]])));
       Object.keys(masterData).forEach((key: string) => masterData[key] = Array.from(new Set(masterData[key])).sort());
       localStorage.setItem(ROUTING_CACHE_MASTER_DATA, JSON.stringify(masterData));
       return masterData as RoutingMasterData;
