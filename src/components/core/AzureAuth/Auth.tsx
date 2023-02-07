f0a2ecc0844e05325aa9ea040db47cd7af85c94d
Eric Phetteplace <eric.phetteplace@libertymutual.com>
2023-02-07 16:54:34 -0500

    Revert "fix: CCTP-2610 fix spread, remove unused interface"
    This reverts commit d9a1ffe4fe1ab46d8d802552e1e950dc1b031494.

--- src/components/core/AzureAuth/Auth.tsx
+++ src/components/core/AzureAuth/Auth.tsx
@@ -21,6 +21,10 @@ export const PERMISSIONS = {
   READ_WRITE_GROUP_ROUTING_PROD: "gpi-cct-config-route-readwrite-prod"
 };
 
+interface MsalEnvironment {
+  authority: string;
+  clientId: string;
+}
 interface TokenRequest {
   scopes: string[];
 }
@@ -93,7 +97,6 @@ export function authWrapper(
         this.getAccessToken();
       }, authErr => { // on fail
         this.setState({
-          ...this.state,
           hasError: true,
           errorMessage: authErr.errorMessage
         });
@@ -103,7 +106,6 @@ export function authWrapper(
       if (msalInstance.isCallback(window.location.hash)) {
         console.info("is a callback");
         this.setState({
-          ...this.state,
           renewIframe: true
         });
         return;
