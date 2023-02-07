99c43b4f77373616e495a5d1bbbdb6e9fac6be35
Eric Phetteplace <eric.phetteplace@libertymutual.com>
2023-02-07 16:42:06 -0500

    fix: CCTP-2610 change DB to new dev-cct DB

--- src/utils/test/configUtils.test.js
+++ src/utils/test/configUtils.test.js
@@ -65,6 +65,6 @@ describe("configUtils.js", ()=>{
   });
   test("Simulate getGraphQLEndpoint", ()=>{
     const endpoint = getGraphQLEndpoint();
-    expect(endpoint).toBe("https://2yooyvouuzeq7evpxtv5ojad3m.appsync-api.us-east-1.amazonaws.com/graphql");
+    expect(endpoint).toBe("https://flyyckd74fauthoxf5fj23ko6m.appsync-api.us-east-1.amazonaws.com/graphql");
   });
 });
