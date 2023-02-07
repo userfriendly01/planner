49daa77705f3396c3be8d7286731527eb1c1f304
Eric Phetteplace <eric.phetteplace@libertymutual.com>
2023-02-07 16:57:32 -0500

    Revert "Revert "fix: CCTP-2610 update grid and modals""
    This reverts commit c35ab1ca9e2dcc9cbbab3ac6ed4a6e6022ff8878.

--- src/components/tabs/alohaRouting/DataGridRouting/DataGridRouting.tsx
+++ src/components/tabs/alohaRouting/DataGridRouting/DataGridRouting.tsx
@@ -42,7 +42,9 @@ export const DataGridRouting = (props: AzureSPA): JSX.Element => {
   const [alertBar, setAlertBar] = useState(initializedAlertBar);
   useEffect(() => {
     const getTableData = async () =>{
-      await loadDataTable();
+      const result: CctSharedCallRoutingDb[] = await retrieveRoutingData(accessToken, graphQlApiUrl);
+
+      await loadDataTable(result);
     };
     getTableData();
   }, []);
@@ -111,9 +113,8 @@ export const DataGridRouting = (props: AzureSPA): JSX.Element => {
     }
   };
 
-  const loadDataTable = async () => {
-    const result: CctSharedCallRoutingDb[] = await retrieveRoutingData(accessToken, graphQlApiUrl);
-    if (result.length > 0) {
+  const loadDataTable = async (result?: CctSharedCallRoutingDb[]) => {
+    if (result?.length > 0) {
       const sortedResult: CctSharedCallRoutingDb[] = result.sort(((a: CctSharedCallRoutingDb, b: CctSharedCallRoutingDb) => a.id - b.id));
       const minId: number = sortedResult[0].id;
       const maxId: number = sortedResult[result.length - 1].id;
@@ -141,8 +142,10 @@ export const DataGridRouting = (props: AzureSPA): JSX.Element => {
     } else {
       setState({
         ...state,
-        data: result,
-        filteredItems: result,
+        ...(result) && {
+          data: result,
+          filteredItems: result
+        },
         fetching: false,
         isAddModalOpen: false,
         isEditModalOpen: false,
@@ -152,18 +155,24 @@ export const DataGridRouting = (props: AzureSPA): JSX.Element => {
   };
 
 
-  const openAddModal = (flag: boolean) => {
+  const openAddModal = (flag: boolean, row?: CctSharedCallRoutingDb) => {
+    let newData;
     if (!flag) {
+      newData = row? state.data.concat(row) : undefined;
+
       setAlertBar(alertBarProps => ({
         ...alertBarProps,
         open: flag,
         severityType: "success",
         msg: "New flow has been successfully added!! "
       }));
-      loadDataTable();
     }
     setState({
       ...state,
+      ...(newData) && {
+        data: newData,
+        filteredItems: newData
+      },
       isAddModalOpen: flag
     });
   };
@@ -201,15 +210,27 @@ export const DataGridRouting = (props: AzureSPA): JSX.Element => {
     });
   };
 
-  const openEditModal = (flag: boolean, isSubmitted?: boolean, row?: CctSharedCallRoutingDb, message?: string) => {
+  const openEditModal = (flag: boolean, isSubmitted?: boolean, row?: CctSharedCallRoutingDb, message?: string, deleteRow?: boolean) => {
+    let newData;
     if (!flag && isSubmitted) {
+      if(deleteRow) {
+        newData = state.data.filter(x=> x.skey !== row.skey);
+      } else {
+        newData = row ? state.data.map(x=> x.skey === row.skey ? row : x) : undefined;
+      }
       setAlertBar((alertBarProps: AlertBarProps) => ({
         ...alertBarProps,
         open: true,
         msg: message,
         severityType: "success"
       }));
-      loadDataTable();
+      setState({
+        ...state,
+        ...(newData) && {
+          data: newData,
+          filteredItems: newData
+        }
+      });
     }
     setState((currentDataRouting: RoutingStateVariables)=>({
       ...currentDataRouting,
