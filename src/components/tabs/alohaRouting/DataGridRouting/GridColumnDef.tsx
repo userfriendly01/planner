e2a4cc10b18411513c5d273eb74e8c2b2311ee22
Ankitha <ankitha.m@libertymutual.com>
2023-02-17 15:51:14 +0530

    added tooltip and ui change for data req

--- src/components/tabs/alohaRouting/DataGridRouting/GridColumnDef.tsx
+++ src/components/tabs/alohaRouting/DataGridRouting/GridColumnDef.tsx
@@ -2,7 +2,8 @@ import {
   GridColDef, GridRenderCellParams
 } from "@mui/x-data-grid";
 import { CctSharedCallRoutingDb } from "../AlohaRouting.Interfaces";
-
+import Tooltip from "@mui/material/Tooltip";
+import React from "react";
 const formatDateTime = (dateTime: string) => {
   if(dateTime.includes("AM") || dateTime.includes("PM")){
     const [time, modifier] = dateTime.split(" ");
@@ -21,92 +22,107 @@ export const RoutingGridColumnDef: GridColDef[] = [
     field: "id",
     sortable: true,
     width: 60,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Brand",
     field: "brand",
     sortable: true,
-    width: 80,
-    align: "center"
+    width: 110,
+    align: "left",
+    renderCell: (params: any) =>  (
+      <Tooltip title={params.row.brand || ""} >
+        <div>{params.row.brand || ""}</div>
+      </Tooltip>
+    )
   },
   {
     headerName: "Caller State",
     field: "callerState",
     sortable: true,
     width: 110,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Caller Type",
     field: "callerType",
     sortable: true,
     width: 110,
-    align: "center"
+    align: "left",
+    renderCell: (params: any) =>  (
+      <Tooltip title={params.row.callerType || ""} >
+        <div>{params.row.callerType || ""}</div>
+      </Tooltip>
+    )
   },
   {
     headerName: "Caller Intent",
     field: "callIntent",
     sortable: true,
     width: 110,
-    align: "center"
+    align: "left",
+    renderCell: (params: any) =>  (
+      <Tooltip title={params.row.callIntent || ""} >
+        <div>{params.row.callIntent || ""}</div>
+      </Tooltip>
+    )
   },
   {
     headerName: "Channel",
     field: "channel",
     sortable: true,
     width: 110,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Day Of Week",
     field: "dayOfWeek",
     sortable: true,
-    align: "center",
+    align: "left",
     width: 80
   },
   {
     headerName: "Transfer Dest.",
     field: "transferDestination",
     sortable: true,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Twilio Skill",
     field: "twilioSkill",
     sortable: true,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "CRC Skill",
     field: "crcSkill",
     sortable: true,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Pct. Of Callers",
     field: "percentOfCallers",
     sortable: true,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Policy Type",
     field: "policyType",
     sortable: true,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Start Time",
     field: "startTime",
     sortable: true,
-    align: "center",
+    align: "left",
     valueGetter: (params: GridRenderCellParams<CctSharedCallRoutingDb>) => (formatDateTime(params.row.startTime))
   },
   {
     headerName: "End Time",
     field: "endTime",
     sortable: true,
-    align: "center",
+    align: "left",
     valueGetter: (params: GridRenderCellParams<CctSharedCallRoutingDb>) => (formatDateTime(params.row.endTime))
   },
 
@@ -114,27 +130,32 @@ export const RoutingGridColumnDef: GridColDef[] = [
     headerName: "Transfer Message",
     field: "transferMessage",
     sortable: false,
-    width: 110
+    width: 110,
+    renderCell: (params: any) =>  (
+      <Tooltip title={params.row.transferMessage || ""} >
+        <div>{params.row.transferMessage || ""}</div>
+      </Tooltip>
+    )
   },
   {
     headerName: "Priority",
     field: "priority",
     sortable: true,
     width: 110,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Occupancy Check",
     field: "occupancyCheck",
     sortable: true,
     width: 110,
-    align: "center"
+    align: "left"
   },
   {
     headerName: "Routing Steps",
     field: "routingSteps",
     sortable: false,
     width: 110,
-    align: "center"
+    align: "left"
   }
 ];
