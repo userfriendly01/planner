f0c74048bdd3e991c01009aa6aceb58eed5ceeeb
Eric Phetteplace <eric.phetteplace@libertymutual.com>
2023-03-31 13:53:28 -0400

    fix: CCTP-3684 switch back to popup auth

--- src/components/tabs/alohaFlow/test/AlohaFlowContainer.test.jsx
+++ src/components/tabs/alohaFlow/test/AlohaFlowContainer.test.jsx
@@ -16,7 +16,7 @@ jest.mock("msal", () => ({
   __esModule: true,
   UserAgentApplication: jest.fn().mockImplementation(() => {
     return {
-      acquireTokenSilent: jest.fn().mockResolvedValue({
+      acquireTokenPopup: jest.fn().mockResolvedValue({
         accessToken: "mockt-test-token-1234"
       }),
       handleRedirectCallback: jest.fn(),
