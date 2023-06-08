ed7cd7cae1e2632a788f912f3f27ba7553e05c68
=Ankitha <=Ankitha.m@libertymutual.com>
2023-06-08 09:50:33 -0400

    Add routing team

--- src/components/tabs/usermanagement/OnboardNewUser/RoutingAttributes/RoutingAttributes.tsx
+++ src/components/tabs/usermanagement/OnboardNewUser/RoutingAttributes/RoutingAttributes.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Accordion, AccordionSummary, AccordionDetails
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import SelectContainer from "components/core/SharedComponents/SelectContainer";
 const RoutingAttributes = ():JSX.Element =>{
+  const [routingTeam,setRoutingTeam] = useState(null);
+  const routingTeamList = ["sample1", "sample2"];
+  const handleChange=(event:any)=>{
+    setRoutingTeam(event.target.value);
+  }
   return (
     <Accordion sx={{
-      width: "384px",
+      width: "395px",
       margin: "8px 0px 5px 0px"
     }}>
       <AccordionSummary
@@ -15,6 +21,17 @@ const RoutingAttributes = ():JSX.Element =>{
         id="panel1a-header"
       >Routing Attributes
       </AccordionSummary>
+      <AccordionDetails>
+      <SelectContainer
+          name= {"Routing Team"}
+          label={"Routing Team"}
+          value={routingTeam}
+          onChange={handleChange}
+          dropDownOptions={routingTeamList}
+          required = {false}
+          isBlankFirstValue={true}
+        />
+      </AccordionDetails>
       <AccordionDetails>
            Upcoming Routing Attributes
       </AccordionDetails>
