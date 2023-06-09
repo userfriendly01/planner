7bcb1a8ae7d3b6e6a2136fbd26f96b7c7eed6120
=Ankitha <=Ankitha.m@libertymutual.com>
2023-06-09 06:07:21 -0400

    Add Routing team

--- src/components/tabs/usermanagement/OnboardNewUser/RoutingAttributes/test/RoutingAttributes.test.jsx
+++ src/components/tabs/usermanagement/OnboardNewUser/RoutingAttributes/test/RoutingAttributes.test.jsx
@@ -1,11 +1,29 @@
-import { render } from "testUtils";
+import { act, render, setupMockedComponents } from "testUtils";
+import {
+  Accordion
+} from "@mui/material";
 import React from "react";
 import { default as RoutingAttributes } from "../RoutingAttributes";
 
+jest.mock("@mui/material", () => {
+  return{
+    __esModule: true,
+    Accordion: jest.fn()
+  };
+});
 
+jest.mock("components", () => {
+  return{
+    __esModule: true,
+    SelectContainer: jest.fn()
+  };
+});
 describe("<RoutingAttributes />", ()=>{
   beforeEach(()=>{
     jest.clearAllMocks();
+  });
+  setupMockedComponents({
+    Accordion
   });
   const renderComponent = () => {
     return render(
@@ -13,7 +31,22 @@ describe("<RoutingAttributes />", ()=>{
     );
   };
   test("Simulate Component", ()=>{
-    const { getByText } = renderComponent();
-    expect(getByText("Upcoming Routing Attributes")).toBeInTheDocument();
+    renderComponent();
+    const upcomingAttributes = Accordion.mock.calls[0][0].children[2].props.children;
+    expect(upcomingAttributes).toBe('Upcoming Routing Attributes');
+  });
+  test("Simulate Routing Team Component", ()=>{
+    const eventOnChange={
+      target: {
+        name: "routing Team",
+        value: "sample1"
+      }
+    }
+    renderComponent();
+    const routingTeamChange = Accordion.mock.calls[0][0].children[1].props.children.props.onChange;
+    act(()=>{
+      routingTeamChange(eventOnChange);
+    })
+    expect(routingTeamChange).toBeTruthy();
   });
 });
