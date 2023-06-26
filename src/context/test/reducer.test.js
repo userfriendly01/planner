e39a9d4e11e9d25036ae46eb9a90251032f1543b
Faith Cuneo <faith.cuneo@libertymutual.com>
2023-06-26 19:25:54 -0400

    fixed reducer tests:

--- src/context/test/reducer.test.js
+++ src/context/test/reducer.test.js
@@ -16,7 +16,7 @@ describe("reducer", () => {
       expect(result).toBe(initialState);
     });
   });
-  describe.only("addManager", () => {
+  describe("addManager", () => {
     test("should add to the managers array", () => {
       const payload = {
         manager_first_name: "joe",
@@ -353,22 +353,37 @@ describe("reducer", () => {
   });
   describe("updateWfmOrg", () => {
     test("should update wfmOrg only", () => {
-      const payload = [
-        {
+      const payload = {
+        org: [{
           id: 1,
           name: "Administrator"
         },
         {
           id: 2,
           name: "Agent"
-        }
-      ];
+        }],
+        errors: []
+      };
       const action = {
         type: "updateWfmOrg",
         payload
       };
       const result = reducer(initialState, action);
-      expect(result.calabrioContext.wfmOrg).toEqual(payload);
+      expect(result.calabrioContext.wfmOrg).toEqual(payload.org);
+      expect(result.calabrioContext.wfmErrors).toEqual(payload.errors);
+    });
+    test("should update wfmOrg only", () => {
+      const payload = {
+        org: undefined,
+        errors: undefined
+      };
+      const action = {
+        type: "updateWfmOrg",
+        payload
+      };
+      const result = reducer(initialState, action);
+      expect(result.calabrioContext.wfmOrg).toEqual([]);
+      expect(result.calabrioContext.wfmErrors).toEqual([]);
     });
   });
   describe("loadWfmOrg", () => {
