468bc34c055daaaa5de15ddcef4fb385a2481216
Mrinal Denre <mrinal.denre@libertymutual.com>
2023-08-23 07:30:30 -0400

    Add Test case for batch Flow Update

--- jestSetup.js
+++ jestSetup.js
@@ -27,6 +27,7 @@ jest.mock("services", () => ({
   addSkillGroup: jest.fn(),
   addSkillGroupSkill: jest.fn(),
   batchDelete: jest.fn(),
+  batchFlowUpdate: jest.fn(),
   flowBatchDelete: jest.fn(),
   checkExtension: jest.fn(),
   createCalabrioTeam: jest.fn(),
