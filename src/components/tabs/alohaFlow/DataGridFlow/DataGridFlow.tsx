e0be2ac3502e4ddf4b78d5f57865ad4ebf6b4c76
Mrinal Denre <mrinal.denre@libertymutual.com>
2023-09-04 07:06:09 -0400

    Create Bulk Flow Code Changes

--- src/components/tabs/alohaFlow/DataGridFlow/DataGridFlow.tsx
+++ src/components/tabs/alohaFlow/DataGridFlow/DataGridFlow.tsx
@@ -12,7 +12,7 @@ import React, {
   useEffect, useState
 } from "react";
 import {
-  queryFlowData, retrieveFlowData, flowBatchDelete, batchFlowUpdate
+  queryFlowData, retrieveFlowData, flowBatchDelete, batchFlowUpdate, batchFlowCreate
 } from "services";
 import {
   CACHE_FILTER_FLOW,
@@ -333,8 +333,34 @@ const DataGridFlow = (props: AzureSPA): JSX.Element => {
     setSelectedList(selectedRowsData);
   };
 
-  const handleOnBulkCreate = (rows: Array<CctSharedCallFlowDb> ) =>{
-    console.log("Bulk Create: ", rows);
+  const handleOnBulkCreate = async(rows: Array<CctSharedCallFlowDb> ) =>{
+    const response = await batchFlowCreate(rows, accessToken, graphQLEndpoint);
+    if(!response || response.errors) {
+      setAlertBar((alertBarProps: AlertBarProps) => ({
+        ...alertBarProps,
+        open: true,
+        msg: "Error while creating the records.",
+        severityType: "error"
+      }));
+      throw new Error("Error while creating the records.");
+    } else {
+      setAlertBar((alertBarProps: AlertBarProps) => ({
+        ...alertBarProps,
+        open: true,
+        msg: "Flow Rules have been successfully created.",
+        severityType: "success"
+      }));
+    }
+
+    const filteredItems = [...dataFlow.filteredItems, ...rows];
+    const filteredData = [...dataFlow.data, ...rows];
+    setSelectedList([]);
+    setDataFlow({
+      ...dataFlow,
+      ...filteredItems && { filteredItems },
+      data: filteredData,
+      isPreviewModalOpen: false
+    });
   };
 
   const handleOnBulkUpdate = async(rows: Array<CctSharedCallFlowDb> ) =>{
@@ -495,6 +521,7 @@ const DataGridFlow = (props: AzureSPA): JSX.Element => {
         rows={selectedList}
         onClose={handlePreviewModalOnClose}
         action={dataFlow.previewModalAction}
+        maxId={dataFlow.maxId}
         onCreate={handleOnBulkCreate}
         onUpdate={handleOnBulkUpdate}
         onDelete={handleOnBulkDelete}
