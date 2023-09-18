41ef2ae2200017ac1a5e6fa60ef1136a7551def0
Faith Cuneo <faith.cuneo@libertymutual.com>
2023-09-18 16:36:13 -0400

    small PR cleanups

--- src/components/tabs/index.js
+++ src/components/tabs/index.js
@@ -1,6 +1,5 @@
 export * from "./usermanagement";
 export * from "./callflowmanagement";
 export * from "./orgmanagement";
-export * from "./orgmanagement/triton";
 export * from "./alohaFlow";
 export * from "./alohaRouting";
--- src/components/tabs/orgmanagement/index.js
+++ src/components/tabs/orgmanagement/index.js
@@ -1,3 +1,4 @@
 export * from "./calabrio/CalabrioTeamModal";
 export { default as CalabrioRolesWrapper } from "./calabrio/CalabrioRolesWrapper/CalabrioRolesWrapper";
 export { default as CalabrioOrgWrapper } from "./calabrio/CalabrioOrgWrapper/CalabrioOrgWrapper";
+export * from "./triton";
