be8259d57cb772a480e599c43ab091caba40e08d
Faith Cuneo <faith.cuneo@libertymutual.com>
2023-10-23 14:51:59 -0400

    trying boo

--- src/components/tabs/usermanagement/CompareProfiles/test/ResetModal.test.jsx
+++ src/components/tabs/usermanagement/CompareProfiles/test/ResetModal.test.jsx
@@ -178,7 +178,7 @@ describe("ResetModal", () => {
             expect(ProgressBar).toHaveBeenCalledTimes(1);
             jest.advanceTimersByTime(6000);
             await waitFor(() => {
-              expect(fetchResetProfileDatadogLogs).toHaveBeenCalledTimes(37);
+              expect(fetchResetProfileDatadogLogs).toHaveBeenCalledTimes(33);
               expect(rendered.container).toHaveTextContent("Calabrio... is the worst we're sorry");
               expect(rendered.container).toHaveTextContent("We waited a while but the log was not found in datadog. We're unable to confirm this process succeeded. Please try again.");
             });
