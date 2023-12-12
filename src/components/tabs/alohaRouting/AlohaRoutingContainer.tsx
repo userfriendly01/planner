8d428362d32f3dedec14e87b3b8e4d507911f970
Mrinal Denre <mrinal.denre@libertymutual.com>
2023-12-12 08:42:23 -0500

    Cached Item Fixed for Flow and Routing

--- src/components/tabs/alohaRouting/AlohaRoutingContainer.tsx
+++ src/components/tabs/alohaRouting/AlohaRoutingContainer.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { DataGridRouting } from "./DataGridRouting";
 import { authWrapper } from "../../core/AzureAuth";
 import { AzureSPA } from "globals";
+import {
+  LAST_ROUTING_MASTER_DATA_CACHED_DATE, ROUTING_CACHE_MASTER_DATA
+} from "utils";
 
 const AlohaRoutingContainer = (props: AzureSPA) => {
   const {
     accessToken,
     matchedGroups
   } = props;
+
+  useEffect(()=>{
+    updateCacheData();
+  },[]);
+
+  const updateCacheData = () =>{
+    const lastFlowMasterDataSet = localStorage.getItem(LAST_ROUTING_MASTER_DATA_CACHED_DATE);
+    if(lastFlowMasterDataSet){
+      const currentDate = new Date();
+      const lastSetDate = new Date(lastFlowMasterDataSet);
+      const difference: number = currentDate.getTime() - lastSetDate.getTime();
+      const differenceInDays = difference / (1000*60*60*24);
+      if(differenceInDays>1){
+        localStorage.removeItem(ROUTING_CACHE_MASTER_DATA);
+      }
+    }
+    else{
+      localStorage.removeItem(ROUTING_CACHE_MASTER_DATA);
+    }
+  };
 
   return (
     <DataGridRouting accessToken={accessToken} matchedGroups={matchedGroups} />
--- src/components/tabs/alohaRouting/DataGridRouting/GridMaster.tsx
+++ src/components/tabs/alohaRouting/DataGridRouting/GridMaster.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-console, max-len,  no-return-assign */
 
 import {
+  LAST_ROUTING_MASTER_DATA_CACHED_DATE,
   ROUTING_CACHE_MASTER_DATA,
   logger
 } from "utils";
@@ -12,16 +13,28 @@ const masterDataItems: string[] = ["channel", "brand", "callerType", "callerStat
 
 const clearGridMasterData = (): void => localStorage.removeItem(ROUTING_CACHE_MASTER_DATA);
 
+const constructMasterData = (data: CctSharedCallRoutingDb[], masterData: Record<string, any> = {}) =>{
+  data.forEach((elem: Record<string, any>) => masterDataItems.forEach((key: string) => (masterData[key] ? masterData[key].push(elem[key]) : masterData[key] = [elem[key]])));
+  Object.keys(masterData).forEach((key: string) => masterData[key] = Array.from(new Set(masterData[key])).sort());
+  return masterData;
+};
+
+const setMasterData = (masterData: Record<string, any>) =>{
+  localStorage.setItem(ROUTING_CACHE_MASTER_DATA, JSON.stringify(masterData));
+  localStorage.setItem(LAST_ROUTING_MASTER_DATA_CACHED_DATE,new Date().toString());
+};
+
 const getGridMasterData = (data: CctSharedCallRoutingDb[]): RoutingMasterData => {
   try {
     const localStorageMasterData: string = localStorage.getItem(ROUTING_CACHE_MASTER_DATA);
     if (localStorageMasterData) {
-      return JSON.parse(localStorageMasterData) as RoutingMasterData;
+      const cachedMasterData = JSON.parse(localStorageMasterData) as RoutingMasterData;
+      const masterData = constructMasterData(data, cachedMasterData);
+      setMasterData(masterData);
+      return masterData;
     } else {
-      const masterData: Record<string, any> = {};
-      data.forEach((elem: Record<string, any>) => masterDataItems.forEach((key: string) => (masterData[key] ? masterData[key].push(elem[key]) : masterData[key] = [elem[key]])));
-      Object.keys(masterData).forEach((key: string) => masterData[key] = Array.from(new Set(masterData[key])).sort());
-      localStorage.setItem(ROUTING_CACHE_MASTER_DATA, JSON.stringify(masterData));
+      const masterData: Record<string, any> = constructMasterData(data);
+      setMasterData(masterData);
       return masterData as RoutingMasterData;
     }
   } catch (error) {
