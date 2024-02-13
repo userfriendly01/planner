afdb310085b33617c80cf224c32f2b264192f95d
Venkateswarl-Onkaram_lmig <venkateswarl.onkaram@libertymutual.com>
2024-02-13 14:04:54 -0500

    Update DynamicFlowContainer.tsx
    Just Tab

--- src/components/tabs/dynamicFlow/DynamicFlowContainer.tsx
+++ src/components/tabs/dynamicFlow/DynamicFlowContainer.tsx
@@ -1,37 +1,8 @@
-import React, { useEffect } from "react";
-import DataGridFlow from "./DataGridFlow/DataGridFlow";
+import React from "react";
 import { authWrapper } from "../../core/AzureAuth";
-import { AzureSPA } from "globals";
-import {
-  FLOW_MASTER_DATA, LAST_FLOW_MASTER_DATA_CACHED_DATE
-} from "utils";
-
-const DynamicFlowContainer = (props:AzureSPA) => {
-  const {
-    accessToken,
-    matchedGroups
-  } = props;
-  useEffect(()=>{
-    updateCacheData();
-  },[]);
-
-  const updateCacheData = () =>{
-    const lastFlowMasterDataSet = localStorage.getItem(LAST_FLOW_MASTER_DATA_CACHED_DATE);
-    if(lastFlowMasterDataSet){
-      const currentDate = new Date();
-      const lastSetDate = new Date(lastFlowMasterDataSet);
-      const difference: number = currentDate.getTime() - lastSetDate.getTime();
-      const differenceInDays = difference / (1000*60*60*24);
-      if(differenceInDays>1){
-        localStorage.removeItem(FLOW_MASTER_DATA);
-      }
-    }
-    else{
-      localStorage.removeItem(FLOW_MASTER_DATA);
-    }
-  };
+const DynamicFlowContainer = () => {
   return (
-    <DataGridFlow accessToken={accessToken} matchedGroups={matchedGroups} />
+    <div>Dynamic Tab</div>
   );
 };
 
