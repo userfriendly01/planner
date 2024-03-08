20abfe7db5a727b3a1da860df63f654d46e053ab
n1557634 <vijay.sidhu@libertymutual.com>
2024-03-08 10:23:20 -0800

    CCTP-11234 added column definations

--- src/components/tabs/dynamicFlow/PreviewModal/TableColumnDef.tsx
+++ src/components/tabs/dynamicFlow/PreviewModal/TableColumnDef.tsx
@@ -6,6 +6,104 @@ export const TableGridColumnDef: GridColDef[] = [
     sortable: true,
     width: 110,
     align: "left"
+  },
+  {
+    headerName: "skey",
+    field: "skey",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "action type",
+    field: "actionType",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "allow bargein",
+    field: "allowBargeIn",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "call flow name",
+    field: "callFlowName",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "finish on key",
+    field: "finishOnKey",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "max digits",
+    field: "maxDigits",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "min digits",
+    field: "minDigits",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "next action",
+    field: "nextActionId",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "next action",
+    field: "nextActionId",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "next action type",
+    field: "nextActionType",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "options",
+    field: "options",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "repeat",
+    field: "repeat",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "speech",
+    field: "speech",
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "timeout",
+    field: "timeout",
+    sortable: true,
+    width: 110,
+    align: "left"
   }
 ];
 
