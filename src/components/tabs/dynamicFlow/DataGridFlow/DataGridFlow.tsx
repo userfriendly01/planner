138f0ce0f76e4df4dfdeeb379494401996a8a758
n1557634 <vijay.sidhu@libertymutual.com>
2024-03-12 07:10:06 -0700

    CCTP-11233 initial code

--- src/components/tabs/dynamicFlow/DataGridFlow/DataGridFlow.tsx
+++ src/components/tabs/dynamicFlow/DataGridFlow/DataGridFlow.tsx
@@ -1,12 +1,20 @@
 import { AzureSPA } from "globals";
-import React, { useState } from "react";
+import React, {
+  useEffect, useState
+} from "react";
 import { CustomFlowGridToolBar } from "../CustomActions/CustomFlowGridToolBar";
 import {
   Action, DynamicStateVariables, PreviewModalAction
 } from "../DynamicFlow.Interfaces";
 import { AlertBarProps } from "utils/interfaces";
-import { initializedAlertBar } from "utils";
+import {
+  getGraphQLEndpoint, initializedAlertBar
+} from "utils";
 import { PreviewModal } from "../PreviewModal/PreviewModal";
+import {
+  queryFlowData, queryLSCDynamicFlowData, retrieveFlowData
+} from "services";
+import { CctSharedCallFlowDb } from "components";
 
 const DataGridFlow = (props: AzureSPA): JSX.Element => {
   const {
@@ -25,7 +33,8 @@ const DataGridFlow = (props: AzureSPA): JSX.Element => {
   const [dataFlow, setDataFlow] = useState(flowInitState);
   const [selectedList, setSelectedList] = useState<Array<Action>>([]);
   const [alertBar, setAlertBar] = useState(initializedAlertBar);
-
+  // Get GraphQL Endpoint. TODO check url
+  const graphQLEndpoint = getGraphQLEndpoint();
   const openPreviewModal = (flag: boolean, action: PreviewModalAction) =>{
     setDataFlow((dataFlowProps: DynamicStateVariables) => ({
       ...dataFlowProps,
@@ -62,6 +71,46 @@ const DataGridFlow = (props: AzureSPA): JSX.Element => {
     // });
     // apiRef.current.setRowSelectionModel([]);
   };
+
+  useEffect(() => {
+    const getTableData = async()=>{
+      const firstChunkData:any = await queryLSCDynamicFlowData(accessToken,  graphQLEndpoint);
+      const listItems = firstChunkData?.data?.getCallFlowConfig?.items || [];
+      let counter =1;
+      const flowData: Action[] = [];
+      listItems.forEach((item: Action) => {
+        if (item) {
+          flowData.push({
+            ...item,
+            id: counter++
+          });
+        }
+      });
+
+      await loadDataTable(flowData);
+      setAlertBar((alertBarProps: AlertBarProps) => ({
+        ...alertBarProps,
+        open: true,
+        severityType: "info",
+        msg: "Data loading in progress. Please wait for the complete set of data to be loaded.",
+        duration: 15000
+      }));
+      const result: CctSharedCallFlowDb[] = await retrieveFlowData(
+        accessToken,
+        graphQLEndpoint,
+        firstChunkData
+      );
+      await loadDataTable(result);
+      setAlertBar((alertBarProps: AlertBarProps) => ({
+        ...alertBarProps,
+        open: true,
+        severityType: "success",
+        msg: "Successfully loaded the flow data!!"
+      }));
+    };
+    getTableData();
+  }, []);
+
   const handlePreviewModalOnClose = () =>{
     setDataFlow((dataFlowProps: DynamicStateVariables) => ({
       ...dataFlowProps,
