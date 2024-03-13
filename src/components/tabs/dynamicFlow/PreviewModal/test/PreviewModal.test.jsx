8958201aa1755bb6b5ffae3758b1c166fe790585
Eric Phetteplace <eric.phetteplace@libertymutual.com>
2024-03-13 09:33:32 -0400

    CCTP-11234 add tests for each component

---
+++ src/components/tabs/dynamicFlow/PreviewModal/test/PreviewModal.test.jsx
@@ -0,0 +1,105 @@
+import {
+  act,
+  fireEvent,
+  initialTestState,
+  render,
+  setupMockedComponents
+} from "testUtils";
+import {
+  PreviewModal
+} from "..";
+import React from "react";
+import {
+  StyledButton
+} from "components";
+import { createFlowDataItem } from "./PreviewUtil.test";
+
+const onCloseMock = jest.fn();
+const onCreateMock = jest.fn();
+const onCreateMockBad = jest.fn().mockImplementation(() => {
+  throw new Error("failed");
+});
+
+jest.mock("components", ()=>({
+  __esModule: true,
+  StyledButton: jest.fn()
+}));
+
+const createFlowDataList = numberOfData =>{
+  const dataList = [];
+  for (let num=1; num<=numberOfData; num++) {
+
+    dataList.push(createFlowDataItem(num));
+  }
+  return dataList;
+};
+
+const renderComponent = (action, createMock) => {
+  return render (<PreviewModal
+    action={action}
+    isOpen={true}
+    onClose={onCloseMock}
+    onCreate={createMock}
+    rows={createFlowDataList(12)}
+  />, initialTestState);
+};
+
+describe("<PreviewModal />", () => {
+  const matchMedia = window.matchMedia;
+  beforeEach(()=>{
+    jest.clearAllMocks();
+    setupMockedComponents({
+      StyledButton
+    });
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn()
+      }))
+    });
+  });
+  afterEach(()=>{
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: matchMedia
+    });
+  });
+  test("render Add preview - create failure", () => {
+    renderComponent("add", onCreateMockBad);
+    const calls = StyledButton.mock.calls;
+    act(async () => calls[0][0].onClick());
+    expect(onCreateMockBad).toBeCalledTimes(1);
+    expect(onCloseMock).toBeCalledTimes(0);
+  });
+  test("render Add preview", () => {
+    const rendered = renderComponent("add", onCreateMock);
+    const calls = StyledButton.mock.calls;
+    expect(calls[2][0].children).toBe("Save");
+    expect(calls[3][0].children).toBe("Cancel");
+    act(() => calls[6][0].onClick());
+    expect(onCreateMock).toBeCalledTimes(1);
+    fireEvent.click(rendered.getByText("Close"));
+    expect(onCloseMock).toBeCalledTimes(1);
+  });
+  test("Add new +", () => {
+    renderComponent("add", onCreateMock);
+    const calls = StyledButton.mock.calls;
+    act(() => calls[4][0].onClick());
+    const calls2 = StyledButton.mock.calls;
+    expect(calls2.length).toBe(12);
+  });
+  // test("Upload CSV File", () => {
+  //   renderComponent("add", onDeleteMock);
+  //   const onChange = StyledButton.mock.calls[5][0].children[0].props.onChange;
+  //   act(() => onChange());
+  //   const calls2 = StyledButton.mock.calls;
+  //   expect(calls2.length).toBe(12);
+  // });
+});
