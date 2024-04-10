f1010e48680f1325d7c3d3dfecc644e91c6e1691
River Marks <River.Marks@LibertyMutual.com>
2024-04-10 09:02:46 -0700

    Updated dynamic flow container no longer use useAccessToken
    Co-authored-by: Julia-Daniels01_lmig <julia.daniels01@libertymutual.com>

--- src/components/tabs/dynamicFlow/DynamicFlowContainer.tsx
+++ src/components/tabs/dynamicFlow/DynamicFlowContainer.tsx
@@ -1,29 +1,17 @@
 import React from "react";
 import DataGridFlow from "./DataGridFlow/DataGridFlow";
-import { useAccessToken } from "authentication";
-import {
-  LoginInProgress,
-  LoginError
-} from "components";
+import { useAdminState } from "context";
 
 const DynamicFlowContainer = () => {
   const {
-    accessToken,
-    matchedGroups,
-    isLoading,
-    error
-  } = useAccessToken();
-
-  if (isLoading) {
-    return <LoginInProgress />;
-  }
-
-  if (error) {
-    return <LoginError message={error} />;
-  }
+    userContext: {
+      permissions,
+      accessToken
+    }
+  } = useAdminState();
 
   return (
-    <DataGridFlow accessToken={accessToken} matchedGroups={matchedGroups} />
+    <DataGridFlow accessToken={accessToken} matchedGroups={permissions} />
   );
 };
 
