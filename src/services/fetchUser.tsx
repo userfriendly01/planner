138b3a30301a4219c73c7f77449b35561bdaade4
River Marks <River.Marks@LibertyMutual.com>
2024-05-08 12:15:33 -0400

    Migrate to discovery

--- src/services/fetchUser.tsx
+++ src/services/fetchUser.tsx
@@ -11,17 +11,28 @@ export interface FetchUserResponse {
   departmentNumber: string
 }
 
-export const fetchUser = (nNumber: string): Promise<FetchUserResponse> => myAxios.get(apiPaths.EMPLOYEE_LOOKUP(nNumber.substring(1)))
+export const fetchUser = (nNumber: string): Promise<FetchUserResponse> => myAxios.get(apiPaths.EMPLOYEE_LOOKUP(nNumber))
   .then(res => {
-    if (res.data.length !== 0) {
+    if (res?.data?.results?.length) {
+      const {
+        preferred_name: preferredName,
+        first_name: firstName,
+        last_name: lastName,
+        employee_email_address: email,
+        office_name: officeName,
+        office_code: officeNumber,
+        dept_name: departmentName,
+        dept_code: departmentNumber
+      } = res.data.results[0];
+
       return {
-        email: res.data[0].person.data.Email,
-        firstName: res.data[0].person.data.FirstName?.trim(),
-        lastName: res.data[0].person.data.LastName?.trim(),
-        officeName: res.data[0].person.data.OfficeName,
-        officeNumber: res.data[0].person.data.OfficeNumber,
-        departmentName: res.data[0].person.data.DepartmentName,
-        departmentNumber: res.data[0].person.data.DepartmentNumber
+        email,
+        firstName: (preferredName || firstName)?.trim(),
+        lastName: lastName?.trim(),
+        officeName,
+        officeNumber,
+        departmentName,
+        departmentNumber
       };
     } else {
       throw "fetchUser employee lookup did not return any data";
