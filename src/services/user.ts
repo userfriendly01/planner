d2cbf12d64840531aff69a16816b9ad74c28b5f9
Faith Cuneo <faith.cuneo@libertymutual.com>
2024-05-28 11:37:17 -0400

    uncommenting console check

--- src/services/user.ts
+++ src/services/user.ts
@@ -38,7 +38,7 @@ export const listUMUsers = async (dispatch: (action: Action) => void): Promise<D
         newUsers.push(
           mapWorkerFromDbWorker({
             ...user,
-            isConsole: false // user.pk.includes("Console")
+            isConsole: user.pk.includes("Console")
           })
         );
       }
