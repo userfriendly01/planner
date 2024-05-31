52db68a82d34d6ca7edddd52b34e9c5d9ad86638
Nidhi Gulati <nidhi.gulati@libertymutual.com>
2024-05-31 09:27:28 -0700

    CCTP-12527 Added bacthDelete test

--- src/components/tabs/alohaFlow/Utils/test/FlowTableServiceUtil.test.js
+++ src/components/tabs/alohaFlow/Utils/test/FlowTableServiceUtil.test.js
@@ -15,7 +15,7 @@ import {
 } from "services";
 
 import {
-  addFlowRule,
+  addFlowRule, batchDeleteActionItems,
   batchDeleteItems,
   batchFlowCreate,
   batchFlowUpdate,
@@ -287,6 +287,23 @@ describe("FlowTableServiceUtil", () => {
       await deleteOppositeRows([dynamicRecord, nonDynamicRecord], token);
 
       expect(v1BatchDeleteItems).toHaveBeenCalled();
+      expect(v2BatchDeleteItems).toHaveBeenCalled();
+    });
+  });
+  describe("batchDeleteActionItems", () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+      v2BatchDeleteItems.mockResolvedValue({
+        failure: ["b"],
+        success: ["a"]
+      });
+    });
+    it("should call batchDeleteActionItems function", async () => {
+      const items = [ {
+        id: 1,
+        actionType: "MENU"
+      }];
+      await batchDeleteActionItems(items, token);
       expect(v2BatchDeleteItems).toHaveBeenCalled();
     });
   });
