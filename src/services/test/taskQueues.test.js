e107b7741674a22d18f402c339373e1b129b5faf
Faith Cuneo <faith.cuneo@libertymutual.com>
2024-06-22 18:42:29 -0400

    tested task queues

--- src/services/test/taskQueues.test.js
+++ src/services/test/taskQueues.test.js
@@ -1,7 +1,12 @@
 import MockAdapter from "axios-mock-adapter";
 import { myAxios } from "utils/myAxios";
 import { apiPaths } from "globals";
-import { getTaskQueues } from "../taskQueues";
+import {
+  getTaskQueues,
+  createTaskQueue,
+  updateTaskQueue,
+  deleteTaskQueue
+} from "../taskQueues";
 
 const axiosMock = new MockAdapter(myAxios);
 
@@ -10,17 +15,16 @@ beforeEach(() => {
   axiosMock.reset();
 });
 
+const taskQueueSid = "TQ32159";
+
 describe("getTaskQueues", () => {
-  test.only("FAITH", () => {
-    expect(true).toBe(true);
-  });
   describe("call succeeds", () => {
     beforeEach(() => axiosMock.onGet(apiPaths.TASK_QUEUES).replyOnce(200, { cool: "beans" }));
     test("should resolve with data", done => {
       getTaskQueues()
         .then(res => {
           expect(axiosMock.history.get.length).toEqual(1);
-          expect(res).toEqual({ cool: "beans" });
+          expect(res.data).toEqual({ cool: "beans" });
           done();
         });
     });
@@ -31,6 +35,81 @@ describe("getTaskQueues", () => {
       getTaskQueues()
         .catch(error => {
           expect(axiosMock.history.get.length).toEqual(1);
+          expect(error).toEqual(new Error("Request failed with status code 500"));
+          done();
+        });
+    });
+  });
+});
+
+describe("createTaskQueue", () => {
+  describe("call succeeds", () => {
+    beforeEach(() => axiosMock.onPost(apiPaths.TASK_QUEUES).replyOnce(200, { cool: "beans" }));
+    test("should resolve with data", done => {
+      createTaskQueue()
+        .then(res => {
+          expect(axiosMock.history.post.length).toEqual(1);
+          expect(res.data).toEqual({ cool: "beans" });
+          done();
+        });
+    });
+  });
+  describe("call fails", () => {
+    beforeEach(() => axiosMock.onPost(apiPaths.TASK_QUEUES).replyOnce(500, { boo: "nooo" }));
+    test("should reject with error", done => {
+      createTaskQueue()
+        .catch(error => {
+          expect(axiosMock.history.post.length).toEqual(1);
+          expect(error).toEqual(new Error("Request failed with status code 500"));
+          done();
+        });
+    });
+  });
+});
+
+describe("updateTaskQueue", () => {
+  describe("call succeeds", () => {
+    beforeEach(() => axiosMock.onPut(`${apiPaths.TASK_QUEUES}/${taskQueueSid}`).replyOnce(200, { cool: "beans" }));
+    test("should resolve with data", done => {
+      updateTaskQueue(taskQueueSid)
+        .then(res => {
+          expect(axiosMock.history.put.length).toEqual(1);
+          expect(res.data).toEqual({ cool: "beans" });
+          done();
+        });
+    });
+  });
+  describe("call fails", () => {
+    beforeEach(() => axiosMock.onPut(`${apiPaths.TASK_QUEUES}/${taskQueueSid}`).replyOnce(500, { boo: "nooo" }));
+    test("should reject with error", done => {
+      updateTaskQueue(taskQueueSid)
+        .catch(error => {
+          expect(axiosMock.history.put.length).toEqual(1);
+          expect(error).toEqual(new Error("Request failed with status code 500"));
+          done();
+        });
+    });
+  });
+});
+
+describe("deleteTaskQueue", () => {
+  describe("call succeeds", () => {
+    beforeEach(() => axiosMock.onDelete(`${apiPaths.TASK_QUEUES}/${taskQueueSid}`).replyOnce(200, { cool: "beans" }));
+    test("should resolve with data", done => {
+      deleteTaskQueue(taskQueueSid)
+        .then(res => {
+          expect(axiosMock.history.delete.length).toEqual(1);
+          expect(res.data).toEqual({ cool: "beans" });
+          done();
+        });
+    });
+  });
+  describe("call fails", () => {
+    beforeEach(() => axiosMock.onDelete(`${apiPaths.TASK_QUEUES}/${taskQueueSid}`).replyOnce(500, { boo: "nooo" }));
+    test("should reject with error", done => {
+      deleteTaskQueue(taskQueueSid)
+        .catch(error => {
+          expect(axiosMock.history.delete.length).toEqual(1);
           expect(error).toEqual(new Error("Request failed with status code 500"));
           done();
         });
