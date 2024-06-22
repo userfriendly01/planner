a601aa1891336b85f172e9a7e47142eab6f69bd9
Kaleigh Spurio <Kaleigh.Spurio@libertymutual.com>
2024-06-22 18:35:22 -0400

    unit tests for createskill and deleteskill

--- src/services/skill.ts
+++ src/services/skill.ts
@@ -35,7 +35,7 @@ export const createSkill = async (skillForm: SkillFormState, updatedBy: string):
       const res = await myAxios.post(apiPaths.TASK_QUEUES, newTaskQueuebody);
       taskQueueSid = res.data.sid;
     } catch(error){
-      const message = `Task Queue failed to create. ${formatError(error?.response?.data || error?.message)}`;
+      const message = `Task Queue failed to create: ${formatError(error?.response?.data || error?.message)}`;
       console.error(message, error);
       messages.push(message);
     }
@@ -66,7 +66,7 @@ export const createSkill = async (skillForm: SkillFormState, updatedBy: string):
 
     await myAxios.post(apiPaths.SKILLS_CALLFLOW, newCallflowSkillBody);
   } catch(error){
-    const message = `Callflow database failed to update: ${formatError(error?.response?.data || error?.message)}`;
+    const message = `Callflow database failed to create skill: ${formatError(error?.response?.data || error?.message)}`;
     console.error(message, error);
     messages.push(message);
   }
