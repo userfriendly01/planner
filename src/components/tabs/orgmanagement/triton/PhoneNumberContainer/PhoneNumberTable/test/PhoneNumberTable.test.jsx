5f6611f5c27a0ccb97bd8c93faa3971728244967
Faith Cuneo <faith.cuneo@libertymutual.com>
2024-07-04 17:06:16 -0400

    fixed PhoneNumberTable tests

--- src/components/tabs/orgmanagement/triton/PhoneNumberContainer/PhoneNumberTable/test/PhoneNumberTable.test.jsx
+++ src/components/tabs/orgmanagement/triton/PhoneNumberContainer/PhoneNumberTable/test/PhoneNumberTable.test.jsx
@@ -5,70 +5,91 @@ import {
   expectMockedComponent,
   expectOnlyPassedProps,
   render,
-  setupMockedComponents,
-  within
+  setupMockedComponents
 } from "testUtils";
+import {
+  Delete,
+  Edit
+} from "@mui/icons-material";
 import { theme } from "globals/theme";
 import { ThemeProvider } from "styled-components";
+import { IconWrapper } from "../../PhoneNumber.Styles";
 
 jest.mock("components/ModalOverlay", () => ({
   ModalOverlay: jest.fn()
 }));
 
-const mockEditButtonOnClick = jest.fn();
-const mockGetDeleteButtonOnClick = jest.fn();
-const emptyListMsg = "yes, we have no directory blah blah blah";
+jest.mock("@mui/icons-material", () => ({
+  Delete: jest.fn(),
+  Edit: jest.fn(),
+  InfoOutlined: jest.fn()
+}));
 
-const renderComponent = (directory, saveState = "whatever") => render(
-  <ThemeProvider theme={theme}>
-    <PhoneNumberTable
-      editFunction={mockEditButtonOnClick}
-      deleteFunction={mockGetDeleteButtonOnClick}
-      emptyListMsg={emptyListMsg}
-      phoneNumberList={directory}
-      saveState={saveState}
-    />
-  </ThemeProvider>
-);
+jest.mock("../../PhoneNumber.Styles", () => ({
+  CustomTable: jest.requireActual("../../PhoneNumber.Styles").CustomTable,
+  CustomTableData: jest.requireActual("../../PhoneNumber.Styles").CustomTableData,
+  CustomTableHeader: jest.requireActual("../../PhoneNumber.Styles").CustomTableHeader,
+  CustomTableRow: jest.requireActual("../../PhoneNumber.Styles").CustomTableRow,
+  IconWrapper: jest.fn(),
+  NoListDiv: jest.requireActual("../../PhoneNumber.Styles").NoListDiv,
+  StyledPaper: jest.requireActual("../../PhoneNumber.Styles").StyledPaper,
+  TableText: jest.requireActual("../../PhoneNumber.Styles").TableText
+}));
+
+const mockEditFunction = jest.fn();
+const mockDeleteFunction = jest.fn();
+
+
 
 describe("<PhoneNumberTable />", () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
     setupMockedComponents({
-      ModalOverlay
+      ModalOverlay,
+      IconWrapper,
+      Edit,
+      Delete
     });
   });
 
-  describe("save status does not exist", () => {
+  describe("type === Directory", () => {
+    const renderComponent = (entries, saveState = "whatever") => render(
+      <ThemeProvider theme={theme}>
+        <PhoneNumberTable
+          editFunction={mockEditFunction}
+          deleteFunction={mockDeleteFunction}
+          type={"Directory"}
+          filteredList={entries}
+          saveState={saveState}
+        />
+      </ThemeProvider>
+    );
     describe("profile does not have entries in its directory", () => {
-      test.only("Faith", () => {
-        expect(true).toBe(true);
-      });
       test("should render emptyListMsg", () => {
         const rendered = renderComponent([]);
-        expect(rendered.container).toHaveTextContent(emptyListMsg);
+        expect(rendered.container).toHaveTextContent("No Directory entries exist for this profile");
       });
     });
     describe("profile has entries in its directory list", () => {
       const directoryList = [
         {
-          directory_id: 168,
-          first_nme: "Mack",
-          last_nme: "Truck",
-          phone_num: "800-123-4567"
+          id: 168,
+          first_name: "Mack",
+          last_name: "Truck",
+          directory_num: "800-123-4567"
         },
         {
-          directory_id: 11,
-          first_nme: "Ford",
-          last_nme: "Ranger",
-          phone_num: "900-555-1212"
+          id: 11,
+          first_name: "Ford",
+          last_name: "Ranger",
+          directory_num: "900-555-1212"
         },
         {
-          directory_id: 2,
-          first_nme: "Dodge",
-          last_nme: "Ram",
-          phone_num: "800-123-4569"
+          id: 2,
+          first_name: "Dodge",
+          last_name: "Ram",
+          directory_num: "800-123-4569"
         }
       ];
       test("should render header and correct info & buttons for each entry", () => {
@@ -78,13 +99,30 @@ describe("<PhoneNumberTable />", () => {
         const tableRows = rendered.getAllByTestId("table-row");
         expect(tableRows.length).toBe(3);
         directoryList.forEach(entry => {
-          expect(rendered.container).toHaveTextContent(entry.first_nme);
-          expect(rendered.container).toHaveTextContent(entry.last_nme);
-          expect(rendered.container).toHaveTextContent(entry.phone_num);
-          const row = rendered.getByText(entry.phone_num).closest("tr");
-          const utils = within(row);
-          expect(utils.getByTestId("edit-button")).toBeInTheDocument();
-          expect(utils.getByTestId("delete-button")).toBeInTheDocument();
+          expect(rendered.container).toHaveTextContent(entry.first_name);
+          expect(rendered.container).toHaveTextContent(entry.last_name);
+          expect(rendered.container).toHaveTextContent(entry.directory_num);
+        });
+        IconWrapper.mock.calls.forEach(call => render(call[0].children));
+        expect(Edit).toHaveBeenCalledTimes(3);
+        expect(Delete).toHaveBeenCalledTimes(3);
+      });
+      describe("edit button is clicked", () => {
+        test("should call mockEditFunction", () => {
+          renderComponent(directoryList);
+          expect(IconWrapper).toHaveBeenCalledTimes(6);
+          const editFunction = IconWrapper.mock.calls[0][0].onClick;
+          editFunction();
+          expect(mockEditFunction).toHaveBeenCalledTimes(1);
+        });
+      });
+      describe("delete button is clicked", () => {
+        test("should call mockEditFunction", () => {
+          renderComponent(directoryList);
+          expect(IconWrapper).toHaveBeenCalledTimes(6);
+          const deleteFunction = IconWrapper.mock.calls[1][0].onClick;
+          deleteFunction();
+          expect(mockDeleteFunction).toHaveBeenCalledTimes(1);
         });
       });
       describe("save status exists", () => {
@@ -105,4 +143,91 @@ describe("<PhoneNumberTable />", () => {
       });
     });
   });
+  describe("type === DialList", () => {
+    const renderComponent = (entries, saveState = "whatever") => render(
+      <ThemeProvider theme={theme}>
+        <PhoneNumberTable
+          editFunction={mockEditFunction}
+          deleteFunction={mockDeleteFunction}
+          type={"DialList"}
+          filteredList={entries}
+          saveState={saveState}
+        />
+      </ThemeProvider>
+    );
+    describe("profile does not have entries in its DialList", () => {
+      test("should render emptyListMsg", () => {
+        const rendered = renderComponent([]);
+        expect(rendered.container).toHaveTextContent("No DialList entries exist for this profile");
+      });
+    });
+    describe("profile has entries in its DialList", () => {
+      const dialList = [
+        {
+          id: 168,
+          contact_name: "Mack Truck",
+          contact_num: "800-123-4567"
+        },
+        {
+          id: 11,
+          contact_name: "Ford Ranger",
+          contact_num: "900-555-1212"
+        },
+        {
+          id: 2,
+          contact_name: "Dodge Ram",
+          contact_num: "800-123-4569"
+        }
+      ];
+      test("should render header and correct info & buttons for each entry", () => {
+        const rendered = renderComponent(dialList);
+        expect(rendered.getByText("NAME", { selector: "th" })).toBeInTheDocument();
+        expect(rendered.getByText("NUMBER", { selector: "th" })).toBeInTheDocument();
+        const tableRows = rendered.getAllByTestId("table-row");
+        expect(tableRows.length).toBe(3);
+        dialList.forEach(entry => {
+          expect(rendered.container).toHaveTextContent(entry.contact_name);
+          expect(rendered.container).toHaveTextContent(entry.contact_num);
+        });
+        IconWrapper.mock.calls.forEach(call => render(call[0].children));
+        expect(Edit).toHaveBeenCalledTimes(3);
+        expect(Delete).toHaveBeenCalledTimes(3);
+      });
+      describe("edit button is clicked", () => {
+        test("should call mockEditFunction", () => {
+          renderComponent(dialList);
+          expect(IconWrapper).toHaveBeenCalledTimes(6);
+          const editFunction = IconWrapper.mock.calls[0][0].onClick;
+          editFunction();
+          expect(mockEditFunction).toHaveBeenCalledTimes(1);
+        });
+      });
+      describe("delete button is clicked", () => {
+        test("should call mockEditFunction", () => {
+          renderComponent(dialList);
+          expect(IconWrapper).toHaveBeenCalledTimes(6);
+          const deleteFunction = IconWrapper.mock.calls[1][0].onClick;
+          deleteFunction();
+          expect(mockDeleteFunction).toHaveBeenCalledTimes(1);
+        });
+      });
+      describe("save status exists", () => {
+        test("should render ModalOverlay with correct props", () => {
+          const message = "don't eat the yellow snow";
+          const status = "peanut butter";
+          const saveState = {
+            overlayMessage: message,
+            status
+          };
+          const rendered = renderComponent(dialList, saveState);
+          expectMockedComponent(rendered, { ModalOverlay });
+          expectOnlyPassedProps(ModalOverlay, {
+            message,
+            status
+          });
+        });
+      });
+    });
+  });
+
 });
