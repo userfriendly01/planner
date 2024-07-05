5cc95ea9ac4f8dd9c508b02e03363259c5b2db2a
Faith Cuneo <faith.cuneo@libertymutual.com>
2024-07-04 22:21:21 -0400

    cleaned up ProfileSettingsContainer tests

--- src/components/tabs/orgmanagement/triton/ProfileSettingsContainer/test/ProfileSettingsContainer.test.jsx
+++ src/components/tabs/orgmanagement/triton/ProfileSettingsContainer/test/ProfileSettingsContainer.test.jsx
@@ -6,18 +6,19 @@ import { ProfileSettingsTable } from "components/tabs/orgmanagement/triton/Profi
 import { ProfileEntryForm } from "orgmanagement/ProfileEntryForm";
 import { Modal } from "@mui/material";
 import { PageLoadSpinner } from "components/PageLoadSpinner";
-import { useAdminDispatch, useAdminState, useSkillDispatch, useSkillState } from "context/appContext";
+import { apolloClient } from "components/core/Auth/SharedGraphAPIProvider";
+import {
+  useAdminDispatch, useAdminState, useSkillState
+} from "context/appContext";
 import React from "react";
-import { act } from "react-dom/test-utils";
-import { loadSoftphoneConfigRelationships } from "services/profile";
-import { loadSkillOptions } from "services/skill";
 import {
   expectMockedComponent,
   render,
   setupMockedComponents,
   initialTestState as initialState,
   initialSkillState,
-  mockProfiles
+  mockProfiles,
+  waitFor
 } from "testUtils";
 import { myAxios } from "utils/myAxios";
 
@@ -39,8 +40,11 @@ jest.mock("components/PageLoadSpinner", () => ({
   PageLoadSpinner: jest.fn()
 }));
 
-jest.mock("services/profile", () => ({
-  loadSoftphoneConfigRelationships: jest.fn()
+jest.mock("components/core/Auth/SharedGraphAPIProvider", () => ({
+  apolloClient: {
+    mutate: jest.fn(),
+    query: jest.fn()
+  }
 }));
 
 jest.mock("services/skill", () => ({
@@ -55,13 +59,6 @@ jest.mock("context/appContext", () => ({
   ProfileEntryFormStateProvider: jest.requireActual("context/appContext").ProfileEntryFormStateProvider
 }));
 
-jest.mock("context/reducer", () => ({
-  default: jest.fn()
-}));
-
-jest.mock("context/userFormReducer", () => ({
-  default: jest.fn()
-}));
 
 jest.mock("@mui/material", () => ({
   Modal: jest.fn()
@@ -70,7 +67,6 @@ jest.mock("@mui/material", () => ({
 jest.mock("authentication/authUtils", () => ({
   checkIfPO: jest.fn()
 }));
-
 
 const initialTestState = {
   ...initialState,
@@ -82,16 +78,20 @@ const initialTestState = {
   }
 };
 
+const mockDispatch = jest.fn();
 describe("<ProfileSettingsContainer />", () => {
 
   beforeEach(() => {
     axiosMock.reset();
     jest.clearAllMocks();
     useAdminState.mockReturnValue(initialTestState);
+    useAdminDispatch.mockReturnValue(mockDispatch);
     useSkillState.mockReturnValue(initialSkillState);
+    apolloClient.query.mockResolvedValue();
     checkIfPO.mockReturnValue(true);
     setupMockedComponents({
       ProfileSettingsTable,
+      PageLoadSpinner,
       StyledButton,
       Modal,
       ProfileEntryForm
@@ -103,28 +103,31 @@ describe("<ProfileSettingsContainer />", () => {
   };
 
   describe("Profile Entry Form Modal", () => {
-    test.only("form should not render on initial state", () => {
-      const rendered = renderComponent();
-      expectMockedComponent(rendered, { ProfileEntryForm }, 0);
+    test("form should not render on initial state", () => {
+      checkIfPO.mockReturnValue(false);
+      renderComponent();
+      expect(PageLoadSpinner).toHaveBeenCalledTimes(1);
     });
-    test("create profile button and profile settings table is shown when dropdown is changed to PROFILE_SETTINGS", () => {
-      const rendered = renderComponent();
-      expectMockedComponent(rendered, { StyledButton }, 1);
-      expectMockedComponent(rendered, { ProfileSettingsTable }, 1);
-    });
-    test("When ProfileEntryForm handleClose is called, setProfileEntryFormState is set to open === false", () => {
-      const rendered = renderComponent();
-      expectMockedComponent(rendered, { ProfileEntryForm }, 0);
-      expect(StyledButton.mock.calls[0][0].children).toBe("Create Profile");
-      act(() => {
+    describe("loading === true", () => {
+      test("create profile button and profile settings table is shown when loading === true", async () => {
+        const rendered = renderComponent();
+        await waitFor(() => expect(StyledButton).toHaveBeenCalledTimes(1));
+        expectMockedComponent(rendered, { StyledButton }, 1);
+        expectMockedComponent(rendered, { ProfileSettingsTable }, 1);
+      });
+      test("When ProfileEntryForm handleClose is called, setProfileEntryFormState is set to open === false", async () => {
+        const rendered = renderComponent();
+        await waitFor(() => expect(StyledButton).toHaveBeenCalledTimes(1));
+        expect(StyledButton.mock.calls[0][0].children).toBe("Create Profile");
         const onClick = StyledButton.mock.calls[0][0].onClick;
         onClick();
-      });
-      render(Modal.mock.calls[0][0].children);
-      expectMockedComponent(rendered, { ProfileEntryForm }, 1);
-      const handleClose = ProfileEntryForm.mock.calls[0][0].handleClose;
-      act(() => {
+        expect(Modal).toHaveBeenCalledTimes(2);
+        render(Modal.mock.calls[1][0].children);
+        expectMockedComponent(rendered, { ProfileEntryForm }, 1);
+        const handleClose = ProfileEntryForm.mock.calls[0][0].handleClose;
         handleClose();
+        const closeModal = Modal.mock.calls[1][0].onClose;
+        closeModal();
       });
     });
   });
