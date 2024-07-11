55bfbaf126d450c6300905a712f40d07344ea5de
Marks, River <River.Marks@LibertyMutual.com>
2024-07-11 09:38:22 -0700

    Addressing token requests that do not have appropriate account permissions (#685)

--- src/components/app/test/App.test.jsx
+++ src/components/app/test/App.test.jsx
@@ -25,7 +25,6 @@ import {
 import { getWorkerProfileId } from "authentication/authUtils";
 import { getFilteredPermissions } from "authentication/authenticationProfiles";
 
-
 delete window.location;
 window.location = { reload: jest.fn() };
 document.getElementById = jest.fn();
@@ -134,6 +133,13 @@ const mockAdminDispatch = jest.fn();
 const mockSkillDispatch = jest.fn();
 const permissions = [adGroupPermissionMapping[0]];
 
+const mockAzureAccount = {
+  idTokenClaims: {
+    roles: ["Admin"],
+    employeeid: "n1234567"
+  }
+};
+
 describe("<App />", () => {
   let loginPopupFunc;
   let acquireTokenSilentFunc;
@@ -166,7 +172,8 @@ describe("<App />", () => {
 
     loginPopupFunc = jest.fn().mockReturnValue({
       accessToken: "Access Token",
-      expiresOn: new Date()
+      expiresOn: new Date(),
+      account: mockAzureAccount
     });
 
     acquireTokenSilentFunc = jest.fn().mockReturnValue({
@@ -176,12 +183,7 @@ describe("<App />", () => {
 
     useMsal.mockReturnValue({
       instance: {
-        getActiveAccount: jest.fn().mockReturnValue({
-          idTokenClaims: {
-            roles: ["Admin"],
-            employeeid: "n1234567"
-          }
-        }),
+        getActiveAccount: jest.fn().mockReturnValue(mockAzureAccount),
         loginPopup: loginPopupFunc,
         acquireTokenSilent: acquireTokenSilentFunc
       }
@@ -244,7 +246,8 @@ describe("<App />", () => {
           }),
           loginPopup: jest.fn().mockReturnValue({
             accessToken: "Access Token",
-            expiresOn: new Date(1704067201000)
+            expiresOn: new Date(1704067201000),
+            account: mockAzureAccount
           }),
           acquireTokenSilent: jest.fn().mockReturnValue({
             accessToken: "Access Token",
@@ -280,6 +283,56 @@ describe("<App />", () => {
             msGraph: "Access Token",
             sharedGraph: "Access Token",
             adminService: "Access Token"
+          }
+        }
+      });
+    });
+
+    test("should not get additional token if user roles does not contain 'Admin'", async () => {
+      useMsal.mockReturnValue({
+        instance: {
+          getActiveAccount: jest.fn().mockReturnValue({
+            idTokenClaims: {
+              roles: ["Not Admin"],
+              employeeid: "n1234567"
+            }
+          }),
+          loginPopup: jest.fn().mockReturnValue({
+            accessToken: "Access Token",
+            expiresOn: new Date(1704067201000),
+            account: {
+              idTokenClaims: {
+                roles: ["Not Admin"],
+                employeeid: "n1234567"
+              }
+            }
+          }),
+          acquireTokenSilent: jest.fn().mockReturnValue({
+            accessToken: "Access Token",
+            expiresOn: new Date(1704067201000)
+          })
+        }
+      });
+
+      const rendered = render(<App />);
+      await waitFor(() => rendered.getByTestId("app-wrapper"));
+
+      expect(mockAdminDispatch).toHaveBeenCalledTimes(2);
+      expect(mockAdminDispatch).toHaveBeenCalledWith({
+        type: "loadUserData",
+        payload: {
+          permissions,
+          isAdmin: false,
+          nNumber: "n1234567",
+          profileId: 0
+        }
+      });
+      expect(mockAdminDispatch).toHaveBeenCalledWith({
+        type: "loadUserData",
+        payload: {
+          tokens: {
+            msGraph: "Access Token",
+            sharedGraph: "Access Token"
           }
         }
       });
@@ -379,7 +432,8 @@ describe("<App />", () => {
       Date.now.mockReturnValue(1704067200000);
       loginPopupFunc.mockReturnValue({
         accessToken: "Access Token",
-        expiresOn: new Date(1704067201000)
+        expiresOn: new Date(1704067201000),
+        account: mockAzureAccount
       });
     });
 
