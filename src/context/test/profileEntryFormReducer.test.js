5ecd4763972c49b59015ec296c1da5d046389020
Maximilian Ganske <maximilian.ganske@libertymutual.com>
2024-07-15 11:10:19 -0700

    Feat: fixed tests

--- src/context/test/profileEntryFormReducer.test.js
+++ src/context/test/profileEntryFormReducer.test.js
@@ -112,7 +112,7 @@ describe("profileEntryFormReducer", () => {
         clickToDial: true,
         eftAuthorization: true,
         claimNumberEdit: true,
-        ftoBackup: undefined,
+        ftoBackup: true,
         overflowSkill: "lscOBDialer1",
         profileName: "Game of Phones",
         transferQueues: [
@@ -169,7 +169,7 @@ describe("profileEntryFormReducer", () => {
         clickToDial: false,
         eftAuthorization: false,
         claimNumberEdit: false,
-        ftoBackup: undefined,
+        ftoBackup: false,
         overflowSkill: null,
         profileName: "USRM Billing & Collections",
         transferQueues: [],
