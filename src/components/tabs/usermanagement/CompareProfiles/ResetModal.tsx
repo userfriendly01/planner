901ebd0d5717edfd1f7f5f2faab8b022cbb54164
Katelyn Yang <katelyn.yang@libertymutual.com>
2024-07-16 15:39:34 -0400

    calling a console.log instead

--- src/components/tabs/usermanagement/CompareProfiles/ResetModal.tsx
+++ src/components/tabs/usermanagement/CompareProfiles/ResetModal.tsx
@@ -89,7 +89,7 @@ export const ResetModal = (props: ResetModalProps) => {
       setResults(res.data);
       setStatus(StatusOptions.SUCCESS);
     } catch (error) {
-      logger.error("THIS IS THE ERROR", {error})
+      console.log(error);
       const isTimeout = error?.response?.data?.message?.toLowerCase().trim() === "read timed out" || error?.response?.data?.error?.message?.toLowerCase().trim() === "endpoint request timed out";
       if (isTimeout) {
         logger.warn("Call to reset profiles timed out. Trying to fetch datadog log", { nNumber });
