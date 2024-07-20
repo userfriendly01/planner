9d7ecf580d0e01102562a66f6e0864ed8627ab51
Theron Lamm <theron.lamm01@libertymutual.com>
2024-07-19 21:59:52 -0400

    CCTP-12286
    fixed mapping issues

--- src/services/flowTableService.ts
+++ src/services/flowTableService.ts
@@ -788,7 +788,7 @@ const batchFlowUpdate = async(items: any, accessToken: string) =>{
  */
 const updateFlowBatchRun = async(items: any, accessToken: string) =>{
   const input = items.map((item: any)=>{
-    return {
+    const record = {
       pkey: item.pkey,
       agentId: item.agentId || "",
       brand: item.brand,
@@ -807,6 +807,7 @@ const updateFlowBatchRun = async(items: any, accessToken: string) =>{
       createTime: item.createTime,
       updateTime: new Date().toISOString(),
       dialedDescription: item.dialedDescription,
+      employeeId: item.employeeId || "",
       accountManager: item.accountManager || "",
       affinityVDN: item.affinityVDN || "",
       callTypeDescription: item.callTypeDescription || "",
@@ -826,6 +827,12 @@ const updateFlowBatchRun = async(items: any, accessToken: string) =>{
       tfnRoutingGroup: item.tfnRoutingGroup || "",
       type: item.phoneNumberType || ""
     };
+
+    if (!record.employeeId) {
+      delete record.employeeId;
+    }
+
+    return record;
   });
   try {
     const fetchResponse = await fetch(env.GRAPH_API_URL, {
@@ -1117,7 +1124,7 @@ const createDynamicFlowRunItem = async(items: any, accessToken: string) =>{
 const createFlowRunItem = async (items: any[], accessToken: string) =>{
   let response;
   const input = items.map(item=>{
-    return {
+    const record = {
       pkey: item.pkey,
       agentId: item.agentId || "",
       brand: item.brand,
@@ -1135,6 +1142,7 @@ const createFlowRunItem = async (items: any[], accessToken: string) =>{
       },
       createTime: item.createTime,
       dialedDescription: item.dialedDescription,
+      employeeId: item.employeeId || "",
       accountManager: item.accountManager || "",
       affinityVDN: item.affinityVDN || "",
       callTypeDescription: item.callTypeDescription || "",
@@ -1143,6 +1151,7 @@ const createFlowRunItem = async (items: any[], accessToken: string) =>{
       callDetails1: item.callDetails1 || "",
       callDetails2: item.callDetails2 || "",
       tollFreeNumber: item.tollFreeNumber || "",
+      tfnRoutingGroup: item.tfnRoutingGroup || "",
       lineOfBusiness: item.lineOfBusiness || "",
       marketingChannel: item.marketingChannel || "",
       whisper: item.whisper || "",
@@ -1153,6 +1162,12 @@ const createFlowRunItem = async (items: any[], accessToken: string) =>{
       predictiveCaller: item.predictiveCaller || false,
       selfServiceIndicator: item.selfServiceIndicator || false
     };
+
+    if (!record.employeeId) {
+      delete record.employeeId;
+    }
+
+    return record;
   });
   try {
     const fetchResponse = await fetch(env.GRAPH_API_URL, {
