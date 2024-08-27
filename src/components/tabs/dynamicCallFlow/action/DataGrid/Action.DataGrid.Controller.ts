f27c15e75e6ed654a2fe0fee2cece10a195bed63
Theron Lamm <theron.lamm01@libertymutual.com>
2024-08-27 16:50:45 -0400

    CCTP-12286
    simplified function

--- src/components/tabs/dynamicCallFlow/action/DataGrid/Action.DataGrid.Controller.ts
+++ src/components/tabs/dynamicCallFlow/action/DataGrid/Action.DataGrid.Controller.ts
@@ -4,20 +4,7 @@ import { ActionRecordType } from "components/tabs/dynamicCallFlow/action//GraphQ
 import { MatchFilter } from "dynamicCallFlowCommon/Util/Array.Util";
 
 export const actionMatchFilter: MatchFilter<ActionRecordType> = (actionRecord1: ActionRecordType, actionRecord2: ActionRecordType): boolean => {
-  if (!actionRecord1 && actionRecord2) {
-    return false;
-  }
-
-  if (actionRecord1 && !actionRecord2) {
-    return false;
-  }
-
-  if (!actionRecord1 && !actionRecord2) {
-    return true;
-  }
-
-  return actionRecord1 && actionRecord2
-    && actionRecord1[ACTION_ID] === actionRecord2[ACTION_ID];
+  return !actionRecord1 || !actionRecord2 ? !actionRecord1 && !actionRecord2 : actionRecord1[ACTION_ID] === actionRecord2[ACTION_ID];
 };
 
 /**
