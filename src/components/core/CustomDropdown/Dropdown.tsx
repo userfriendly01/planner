a4fc4f602e92142882fcc7aeb34593af5ef8d7b6
Julia-Daniels01_lmig <julia.daniels01@libertymutual.com>
2024-08-29 16:01:43 -0400

    reverting this

--- src/components/core/CustomDropdown/Dropdown.tsx
+++ src/components/core/CustomDropdown/Dropdown.tsx
@@ -45,15 +45,11 @@ export const Dropdown = (props: any) => {
   };
 
   const handleCheckEqual = (option: any, value: any) => {
-    const optionValue = typeof option === "object" ? option?.value : option;
-    const selectedValue = typeof value === "object" ? value?.value : value;
-
-    return optionValue === selectedValue || selectedValue === "";
-    // if(typeof value === "object") {
-    //   return option?.value === value?.value || value?.value === "";
-    // } else {
-    //   return option?.value === value || option?.label === value || value === "";
-    // }
+    if(typeof value === "object") {
+      return option?.value === value?.value || value?.value === "";
+    } else {
+      return option?.value === value || option?.label === value || value === "";
+    }
   };
 
   return (
