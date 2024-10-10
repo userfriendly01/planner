efdaddc66c15c52df35fa1eac6eece494affc1be
Bhopal Singh <bhopal.singh@libertymutual.com>
2024-10-10 08:43:41 -0400

    feat(CallFlow -> PreviewModal): Update Preview Modal column definitions to contain capture type fields - `Endpoint`, `Parameter`, `Outcomes`, and `Valid Lengths` - CCTP-14190

--- src/components/tabs/dynamicCallFlow/action/PreviewModal/Action.Preview.Modal.ColumnDef.tsx
+++ src/components/tabs/dynamicCallFlow/action/PreviewModal/Action.Preview.Modal.ColumnDef.tsx
@@ -1,6 +1,8 @@
-import { Tooltip } from "@mui/material";
+import { ENDPOINT, OUTCOMES, PARAMETER, VALID_LENGTHS } from "../Form/ActionFields";
+
 import { GridColDef } from "@mui/x-data-grid";
 import React from "react";
+import { Tooltip } from "@mui/material";
 
 export const TableGridColumnDef: GridColDef[] = [
   {
@@ -65,6 +67,39 @@ export const TableGridColumnDef: GridColDef[] = [
     sortable: true,
     width: 110,
     align: "left"
+  },
+  {
+    headerName: "Endpoint",
+    field: ENDPOINT,
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "Parameter",
+    field: PARAMETER,
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "Valid Lengths",
+    field: VALID_LENGTHS,
+    sortable: true,
+    width: 110,
+    align: "left"
+  },
+  {
+    headerName: "Outcomes",
+    field: OUTCOMES,
+    sortable: true,
+    width: 330,
+    align: "left",
+    renderCell: (params: any) =>  (
+      <Tooltip title={JSON.stringify(params.row.outcomes) || ""} >
+        <div>{JSON.stringify(params.row.outcomes) || ""}</div>
+      </Tooltip>
+    )
   },
   {
     headerName: "Menu Options",
