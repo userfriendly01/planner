84b21a53e1622afc3023a189c64d0a21bd42ea5d
Kaleigh Spurio <Kaleigh.Spurio@libertymutual.com>
2024-11-20 11:37:45 -0500

    comment

--- src/utils/calabrioUtils.ts
+++ src/utils/calabrioUtils.ts
@@ -93,6 +93,8 @@ const getCalabrioGroupsWithQMFNOLView = (): number[] => {
 };
 
 // Only users that are in teams within particular Parent Groups should have FNOL view, and they should only have the FNOL QM View if they are not managers (ie, people with FNOL view should not have scopes)
+// Single add/edit user scopes come in as CalabrioGroup[]
+// Bulk add/edit user scopes come in as number[]
 export const shouldHaveFNOLQMView = (team: CalabrioGroup | undefined, scope: {groups: CalabrioGroup[] | number[], teams: CalabrioGroup[] | number[]}): boolean => {
 
   const standardizeScopeTeamAndGroup = (scopeArr: CalabrioGroup[] | number[]): number[] => {
